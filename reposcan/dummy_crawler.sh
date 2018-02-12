1b41392cdae05e1fe3df1b4d3d4429d940dbb8d2
Jan Dobes <jdobes@redhat.com>
2018-02-12 16:16:30 +0100

    sync repositories at once

--- reposcan/dummy_crawler.sh
+++ reposcan/dummy_crawler.sh
@@ -1,28 +1,7 @@
 #!/bin/bash
 
 while true; do
-	REPOS=(http://pulp-read.dist.prod.ext.phx2.redhat.com/content/dist/rhel/server/7/7Server/x86_64/os/ \
-		http://pulp-read.dist.prod.ext.phx2.redhat.com/content/dist/rhel/server/7/7.4/x86_64/os/ \
-		http://pulp-read.dist.prod.ext.phx2.redhat.com/content/dist/rhel/server/7/7Server/x86_64/optional/os/ \
-		http://pulp-read.dist.prod.ext.phx2.redhat.com/content/dist/rhel/server/7/7.4/x86_64/optional/os/ \
-		http://pulp-read.dist.prod.ext.phx2.redhat.com/content/dist/rhel/server/6/6Server/x86_64/os/ \
-		http://pulp-read.dist.prod.ext.phx2.redhat.com/content/dist/rhel/server/6/6.9/x86_64/os/ \
-		http://pulp-read.dist.prod.ext.phx2.redhat.com/content/dist/rhel/server/6/6Server/x86_64/optional/os/ \
-		http://pulp-read.dist.prod.ext.phx2.redhat.com/content/dist/rhel/server/6/6.9/x86_64/optional/os/ \
-		http://pulp-read.dist.prod.ext.phx2.redhat.com/content/dist/rhel/workstation/7/7Workstation/x86_64/os/ \
-		http://pulp-read.dist.prod.ext.phx2.redhat.com/content/dist/rhel/workstation/7/7.4/x86_64/os/ \
-		http://pulp-read.dist.prod.ext.phx2.redhat.com/content/dist/rhel/workstation/7/7Workstation/x86_64/optional/os/ \
-		http://pulp-read.dist.prod.ext.phx2.redhat.com/content/dist/rhel/workstation/7/7.4/x86_64/optional/os/ \
-		http://pulp-read.dist.prod.ext.phx2.redhat.com/content/dist/rhel/workstation/6/6Workstation/x86_64/os/ \
-		http://pulp-read.dist.prod.ext.phx2.redhat.com/content/dist/rhel/workstation/6/6.9/x86_64/os/ \
-		http://pulp-read.dist.prod.ext.phx2.redhat.com/content/dist/rhel/workstation/6/6Workstation/x86_64/optional/os/ \
-		http://pulp-read.dist.prod.ext.phx2.redhat.com/content/dist/rhel/workstation/6/6.9/x86_64/optional/os/)
-
-
-	for r in ${REPOS[*]}; do
-		cd /vmaas-reposcan && ./reposcan.py -d $POSTGRES_DB -H $POSTGRES_HOST -U $POSTGRES_USER -P $POSTGRES_PASSWORD -r $r
-	done
-
+	cd /vmaas-reposcan && ./reposcan.py -d $POSTGRES_DB -H $POSTGRES_HOST -U $POSTGRES_USER -P $POSTGRES_PASSWORD --repofile dummy_repolist.txt
 	# sleep for an hour
 	sleep 3600
 done
