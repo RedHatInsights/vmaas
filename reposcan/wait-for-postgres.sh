bd65e299f8cfcb29885a03a6a18aa772a9b4d4f2
Jan Dobes <jdobes@redhat.com>
2018-02-13 11:05:59 +0100

    wait for PostgreSQL in reposcan

---
+++ reposcan/wait-for-postgres.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+# wait-for-postgres.sh
+# modified version from https://docs.docker.com/compose/startup-order/
+
+set -e
+
+cmd="$@"
+
+until PGPASSWORD="$POSTGRES_PASSWORD" psql -h "$POSTGRES_HOST" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c '\q'; do
+  >&2 echo "PostgreSQL is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "PostgreSQL is up - executing command"
+exec $cmd
