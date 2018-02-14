9ced3b924c02e20abd6df15e4caa461ce137eb12
Jan Dobes <jdobes@redhat.com>
2018-02-14 16:40:24 +0100

    add travis.yml, run pylint and summarize return code
    run tests for reposcan on different Python versions

--- reposcan/run_tests.sh
+++ reposcan/run_tests.sh
@@ -4,16 +4,34 @@
 # Go to script's directory
 cd $(dirname $0)
 
+rc=0
+
 # Check for python test files with invalid names
 test_dirs=$(find . -type d -name 'test')
 for test_dir in $test_dirs; do
     test_files=$(find $test_dir -name '*.py' | grep -v "__init__")
     for test_file in $test_files; do
         base_filename=$(basename $test_file)
-        [[ ! "$base_filename" =~ ^test_.* ]] && echo "ERROR: Invalid test file name - $test_file" 
+        [[ ! "$base_filename" =~ ^test_.* ]] && echo "ERROR: Invalid test file name - $test_file" && rc=$(($rc+1))
     done
 done
 
+if [ "$TRAVIS_PYTHON_VERSION" != "" ]; then
+    # Use always "python" executable for all Python versions in Travis
+    py_cmd="python"
+elif [ -f /usr/bin/python3 ]; then
+    py_cmd="python3"
+else
+    py_cmd="python"
+fi
 # Find and run tests
-python3 -m unittest discover -v
+$py_cmd -m unittest discover -v
+rc=$(($rc+$?))
+
+# Run pylint
+find . -iname '*.py' | xargs pylint
+rc=$(($rc+$?))
+
+exit $rc
 )
+
