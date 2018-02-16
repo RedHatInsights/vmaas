77a54a0af59c89102d3e47a2ee29711359c67df3
Michael Mraka <michael.mraka@redhat.com>
2018-02-16 14:37:19 +0100

    silence psql retries
    fixing
    vmaas_reposcan | psql: could not connect to server: Connection refused
    vmaas_webapp | psql: could not connect to server: Connection refused

--- webapp/wait-for-postgres.sh
+++ webapp/wait-for-postgres.sh
@@ -6,7 +6,7 @@ set -e
 
 cmd="$@"
 
-until PGPASSWORD="$POSTGRES_PASSWORD" psql -h "$POSTGRES_HOST" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c '\q'; do
+until PGPASSWORD="$POSTGRES_PASSWORD" psql -h "$POSTGRES_HOST" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c '\q' -q 2>/dev/null; do
   >&2 echo "PostgreSQL is unavailable - sleeping"
   sleep 1
 done
