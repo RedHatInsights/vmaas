e16cd5e90fe2bff1d16412d20c1a5ff03e8074d5
Michael Mraka <michael.mraka@redhat.com>
2018-03-12 10:51:23 +0100

    moved updates API methods into a class

--- webapp/app.py
+++ webapp/app.py
@@ -8,9 +8,7 @@ import os
 from database import Database
 from cve import CveAPI
 from repos import RepoAPI
-import updates
-
-cursor = Database().cursor()
+from updates import UpdatesAPI
 
 
 class DocHandler(tornado.web.RequestHandler):
@@ -52,10 +50,10 @@ class JsonHandler(tornado.web.RequestHandler):
 
 class UpdatesHandler(JsonHandler):
     def process_string(self, data):
-        return updates.process_list(cursor, {'package_list': [data]})
+        return self.application.updatesapi.process_list({'package_list': [data]})
 
     def process_list(self, data):
-        return updates.process_list(cursor, data)
+        return self.application.updatesapi.process_list(data)
 
 class CVEHandler(JsonHandler):
     def process_string(self, data):
@@ -82,6 +80,8 @@ class Application(tornado.web.Application):
             (r"/api/v1/repos/?", ReposHandler),
             (r"/api/v1/repos/[a-zA-Z0-9*-]+/?", ReposHandler)
         ]
+        cursor = Database().cursor()
+        self.updatesapi = UpdatesAPI(cursor)
         self.cveapi = CveAPI(cursor)
         self.repoapi = RepoAPI(cursor)
         tornado.web.Application.__init__(self, handlers)
