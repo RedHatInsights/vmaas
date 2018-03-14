1f702294469dd86164b8a71d8229887e94aecf2e
Michael Mraka <michael.mraka@redhat.com>
2018-03-14 14:36:39 +0100

    move package name splitting into shared module

--- webapp/utils.py
+++ webapp/utils.py
@@ -2,9 +2,32 @@
 Set of functions and precedures shared between different modules.
 """
 
+import re
+
 def join_packagename(name, epoch, version, release, arch):
     """
     Build a package name from the separate NEVRA parts
     """
     epoch = str(epoch) + ':' if int(epoch) else ''
     return "%s-%s%s-%s.%s" % (name, epoch, version, release, arch)
+
+NEVRA_RE = re.compile(r'(.*)-(([0-9]+):)?([^-]+)-([^-]+)\.([a-z0-9_]+)')
+def split_packagename(filename):
+    """
+    Split rpm name (incl. epoch) to NEVRA components.
+
+    Return a name, epoch, version, release, arch, e.g.::
+        foo-1.0-1.i386.rpm returns foo, 0, 1.0, 1, i386
+        bar-1:9-123a.ia64.rpm returns bar, 1, 9, 123a, ia64
+    """
+
+    if filename[-4:] == '.rpm':
+        filename = filename[:-4]
+
+    match = NEVRA_RE.match(filename)
+    if not match:
+        return '', '', '', '', ''
+
+    name, _, epoch, version, release, arch = match.groups()
+    epoch = int(epoch) if epoch is not None else 0
+    return name, epoch, version, release, arch
