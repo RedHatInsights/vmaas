9fb952803355fc0d7f6e4e8cbd8a13eaea910d14
Michael Mraka <michael.mraka@redhat.com>
2018-03-26 15:26:51 +0200

    test fixes - sort json output to make it diff-able

--- webapp/test/test-api.sh
+++ webapp/test/test-api.sh
@@ -8,12 +8,28 @@ TEST_API_URL="http://localhost:8080/api/v1"
 TEST_OUT=$(mktemp /tmp/vmaas-test-out.XXXXXX.json)
 TEST_APIS="cves errata repos updates"
 
+json_format () {
+    python -c 'import sys, json
+def sortlists(o):
+    if type(o) == list:
+        o.sort()
+        for i in o:
+            sortlists(i)
+    elif type(o) == dict:
+        for i in o.values():
+            sortlists(i)
+
+data = json.load(sys.stdin)
+sortlists(data)
+print(json.dumps(data, sort_keys=True, indent=4, separators=(",", ": ")));'
+}
+
 fail=0
 for api in $TEST_APIS ; do
         for data_in in data/"$api".in.*.json ; do
                 echo Testing $(basename "$data_in")
                 data_out="${data_in/.in./.out.}"
-                $TEST_COMMAND -d "@$data_in" "$TEST_API_URL/$api" | json_reformat >"$TEST_OUT"
+                $TEST_COMMAND -d "@$data_in" "$TEST_API_URL/$api" | json_format >"$TEST_OUT"
                 diff -u "$data_out" "$TEST_OUT" || let $((fail++))
         done
 done
