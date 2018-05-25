12a9bc8892565aeed900bd46931912f85def152f
Michael Mraka <michael.mraka@redhat.com>
2018-05-25 13:05:01 +0200

    updated /repo to use disk dump cache

--- webapp/repos.py
+++ webapp/repos.py
@@ -2,95 +2,15 @@
 Module to handle /repos API calls.
 """
 
-from utils import ListDict, format_datetime
+import re
 
-
-class RepoDict(dict):
-    """Dictionary to hold repository data, the extra id attribute
-       is not supposed to be passed to json output."""
-    def __init__(self, oid, *args):
-        dict.__init__(self, *args)
-        self.oid = oid
-
-class RepoCache(object):
-    """Cache which hold repository data and id->label, label->id mappings."""
-    def __init__(self, cursor):
-        self.cursor = cursor
-        self.cache_id = {}
-        self.cache_label = ListDict()
-        self.cache_repo_product_id = {}
-        self.cache_product_repo_id = ListDict()
-        self.prepare()
-
-    def prepare(self):
-        """Read repository data into memory."""
-        self.cursor.execute("""SELECT r.id,
-                                      cs.label,
-                                      cs.name as repo_name,
-                                      r.url,
-                                      a.name as basearch_name,
-                                      r.releasever,
-                                      p.name as product_name,
-                                      p.id as product_id,
-                                      r.revision
-                                 FROM repo r
-                                 JOIN content_set cs ON cs.id = r.content_set_id
-                                 JOIN arch a ON a.id = r.basearch_id
-                                 JOIN product p ON p.id = cs.product_id
-                                 """)
-
-        for oid, label, name, url, basearch, releasever, product, product_id, revision in self.cursor.fetchall():
-            repo_obj = RepoDict(oid, {
-                "label": label,
-                "name": name,
-                "url": url,
-                "basearch": basearch,
-                "releasever": releasever,
-                "product": product,
-                "revision": format_datetime(revision),
-                })
-            self.cache_id[oid] = repo_obj
-            self.cache_label[label] = repo_obj
-            # repo_id -> product_id
-            self.cache_repo_product_id[oid] = product_id
-            # product_id -> [repo_id]
-            self.cache_product_repo_id[product_id] = oid
-
-    def id2label(self, oid):
-        """Repository id->label mapping."""
-        return self.cache_id.get(oid, {}).get("label", None)
-
-    def label2ids(self, label):
-        """Repository label->[id,...] mapping."""
-        return [repo.oid for repo in self.cache_label.get(label, [])]
-
-    def get_by_id(self, oid):
-        """Complete repository data for given id."""
-        return self.cache_id.get(oid, None)
-
-    def get_by_label(self, label):
-        """Complete repository data for given label."""
-        return self.cache_label.get(label, None)
-
-    def all_ids(self):
-        """IDs of all known repositories."""
-        return self.cache_id.keys()
-
-    def id2productid(self, oid):
-        """Repository id -> Product id mapping."""
-        return self.cache_repo_product_id.get(oid, None)
-
-    def productid2ids(self, product_id):
-        """Product id -> [repo_id,...] mapping."""
-        return self.cache_product_repo_id.get(product_id, [])
+from cache import REPO_NAME, REPO_URL, REPO_BASEARCH, REPO_RELEASEVER, REPO_PRODUCT, REPO_REVISION
 
 
 class RepoAPI(object):
     """ Main /repos API class."""
-    # pylint: disable=too-few-public-methods
-    def __init__(self, cursor, repocache):
-        self.cache = repocache
-        self.cursor = cursor
+    def __init__(self, cache):
+        self.cache = cache
 
     def find_repos_by_regex(self, repo_regex):
         """
@@ -100,10 +20,7 @@ class RepoAPI(object):
 
         :returns: list of repository-labels matching the provided regex
         """
-        repo_query = """SELECT label FROM content_set WHERE label ~ %s"""
-        self.cursor.execute(repo_query, [repo_regex])
-        repos = [x[0] for x in self.cursor.fetchall()]
-        return repos
+        return [label for label in self.cache.repolabel2ids if re.match(repo_regex, label)]
 
     def process_list(self, data):
         """
@@ -123,7 +40,17 @@ class RepoAPI(object):
             repos = self.find_repos_by_regex(repos[0])
 
         for label in repos:
-            repolist[label] = self.cache.get_by_label(label)
+            for repo_id in self.cache.repolabel2ids[label]:
+                repo_detail = self.cache.repo_detail[repo_id]
+                repolist.setdefault(label, []).append({
+                    "label": label,
+                    "name": repo_detail[REPO_NAME],
+                    "url": repo_detail[REPO_URL],
+                    "basearch": repo_detail[REPO_BASEARCH],
+                    "releasever": repo_detail[REPO_RELEASEVER],
+                    "product": repo_detail[REPO_PRODUCT],
+                    "revision": repo_detail[REPO_REVISION]
+                    })
 
         response = {
             'repository_list': repolist,
