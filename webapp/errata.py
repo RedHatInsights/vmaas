725443a069954142f07d2393f89e331cb4e82206
Eric Herget <eherget@redhat.com>
2018-06-06 01:08:22 -0400

    Make errata api use Phase II design

--- webapp/errata.py
+++ webapp/errata.py
@@ -2,73 +2,36 @@
 Module contains classes for returning errata data from DB
 """
 
-from utils import format_datetime, parse_datetime, join_packagename
+import re
 
+from utils import format_datetime, parse_datetime, none2empty, join_packagename
+from cache import ERRATA_SYNOPSIS, ERRATA_SUMMARY, ERRATA_TYPE, \
+                  ERRATA_SEVERITY, ERRATA_DESCRIPTION, ERRATA_SOLUTION, \
+                  ERRATA_ISSUED, ERRATA_UPDATED, ERRATA_CVE, ERRATA_PKGIDS, \
+                  ERRATA_BUGZILLA, ERRATA_REFERENCE, ERRATA_URL
 
 class ErrataAPI(object):
     """ Main /errata API class. """
-    def __init__(self, database):
-        self.cursor = database.dictcursor()
+    def __init__(self, cache):
+        self.cache = cache
 
-    def set_cves_for_errata(self, errata_map):
-        """
-        Populate the errata in the given errata_map with each erratum's
-        list of cves.
-        """
-        cve_query = """SELECT errata_cve.errata_id as erratum_id,
-                              cve.name as cve_name
-                         FROM cve
-                         JOIN errata_cve ON cve_id = cve.id
-                        WHERE errata_cve.errata_id IN %s"""
-        self.cursor.execute(cve_query, [tuple(errata_map.keys())])
-        results = self.cursor.fetchall()
-        for result in results:
-            errata_map[result['erratum_id']]['cve_list'].append(result['cve_name'])
+    def find_errata_by_regex(self, regex):
+        """Returns list of errata matching a provided regex."""
+        return [label for label in self.cache.errata_detail
+                if re.match(regex, label)]
 
-    def set_packages_for_errata(self, errata_map):
+    def pkgidlist2packages(self, pkgid_list):
         """
-        Populate the errata in the given errata_map with each erratum's
-        list of packages.
+        This method returns a list of packages for the given list of package ids.
         """
-        pkg_query = """SELECT pkg_errata.errata_id as erratum_id,
-                              package_name.name as name,
-                              evr.epoch as epoch,
-                              evr.version as version,
-                              evr.release as release,
-                              arch.name as arch
-                         FROM pkg_errata
-                         JOIN package ON package.id = pkg_errata.pkg_id
-                         JOIN package_name ON package_name.id = package.name_id
-                         JOIN evr ON evr.id = package.evr_id
-                         JOIN arch ON arch.id = package.arch_id
-                        WHERE pkg_errata.errata_id IN %s"""
-        self.cursor.execute(pkg_query, [tuple(errata_map.keys())])
-        results = self.cursor.fetchall()
-        for result in results:
-            packagename = join_packagename(result['name'],
-                                           result['epoch'],
-                                           result['version'],
-                                           result['release'],
-                                           result['arch'])
-            errata_map[result['erratum_id']]['package_list'].append(packagename)
-
-    def set_references_for_errata(self, errata_map):
-        """
-        Populate the errata in the given errata_map with each erratum's
-        list of references (bugzilla and other).
-        """
-        refs_query = """SELECT errata_id as erratum_id,
-                               type as ref_type,
-                               name
-                          FROM errata_refs
-                         WHERE errata_id IN %s"""
-        self.cursor.execute(refs_query, [tuple(errata_map.keys())])
-        results = self.cursor.fetchall()
-        for result in results:
-            if result['ref_type'] == 'bugzilla':
-                errata_map[result['erratum_id']]['bugzilla_list'].append(result['name'])
-            else:
-                errata_map[result['erratum_id']]['reference_list'].append(result['name'])
+        pkg_list = []
+        cch = self.cache
+        for pkg_id in pkgid_list:
+            name = cch.id2packagename[cch.package_details[pkg_id][0]]
+            epoch, ver, rel = cch.id2evr[cch.package_details[pkg_id][1]]
+            arch = cch.id2arch[cch.package_details[pkg_id][2]]
+            pkg_list.append(join_packagename(name, epoch, ver, rel, arch))
+        return pkg_list
 
     def process_list(self, data):
         """
@@ -81,7 +44,9 @@ class ErrataAPI(object):
         """
 
         modified_since = data.get("modified_since", None)
+        modified_since_dt = parse_datetime(modified_since)
         errata_to_process = data.get("errata_list", None)
+
         response = {"errata_list": {}}
         if modified_since:
             response["modified_since"] = modified_since
@@ -89,54 +54,34 @@ class ErrataAPI(object):
         if not errata_to_process:
             return response
 
-        errata_to_process = list(filter(None, errata_to_process))
-        # Select all errata in request
-        errata_query = """SELECT errata.id as oid,
-                                 errata.name as name,
-                                 synopsis,
-                                 summary,
-                                 errata_type.name as type,
-                                 errata_severity.name as severity,
-                                 description,
-                                 solution,
-                                 issued,
-                                 updated
-                            FROM errata
-                            JOIN errata_type ON errata_type_id = errata_type.id
-                            JOIN errata_severity ON severity_id = errata_severity.id
-                           WHERE"""
-
         if len(errata_to_process) == 1:
-            errata_query += " errata.name ~ %s"
-            errata_query_params = errata_to_process
-        else:
-            errata_query += " errata.name IN %s"
-            errata_query_params = [tuple(errata_to_process)]
-        if modified_since:
-            errata_query += " and errata.updated >= %s"
-            errata_query_params.append(parse_datetime(modified_since))
-        self.cursor.execute(errata_query, errata_query_params)
-        errata_results = self.cursor.fetchall()
+            # treat single-label like a regex, get all matching names
+            errata_to_process = self.find_errata_by_regex(errata_to_process[0])
 
-        errata_map = {} # map keyed on oid for populating lists in errata
-        response_dict = {} # map keyed on erratum name to return to caller
-        for erratum in errata_results:
-            oid = erratum.pop('oid')
-            erratum_name = erratum.pop('name')
-            erratum['updated'] = format_datetime(erratum['updated'])
-            erratum['issued'] = format_datetime(erratum['issued'])
-            erratum['bugzilla_list'] = []
-            erratum['package_list'] = []
-            erratum['reference_list'] = []
-            erratum['cve_list'] = []
-            erratum['url'] = "https://access.redhat.com/errata/%s" % erratum_name
-            errata_map[oid] = erratum
-            response_dict[erratum_name] = erratum
+        errata_list = {}
+        for errata in errata_to_process:
+            errata_detail = self.cache.errata_detail.get(errata, None)
+            if not errata_detail:
+                continue
 
-        if errata_map:
-            self.set_cves_for_errata(errata_map)
-            self.set_packages_for_errata(errata_map)
-            self.set_references_for_errata(errata_map)
+            if modified_since and (errata_detail[ERRATA_UPDATED] < modified_since_dt
+                                   and errata_detail[ERRATA_ISSUED] < modified_since_dt):
+                continue
 
-        response["errata_list"] = response_dict
+            errata_list[errata] = {
+                "synopsis": none2empty(errata_detail[ERRATA_SYNOPSIS]),
+                "summary": none2empty(errata_detail[ERRATA_SUMMARY]),
+                "type": none2empty(errata_detail[ERRATA_TYPE]),
+                "severity": none2empty(errata_detail[ERRATA_SEVERITY]),
+                "description": none2empty(errata_detail[ERRATA_DESCRIPTION]),
+                "solution": none2empty(errata_detail[ERRATA_SOLUTION]),
+                "issued": none2empty(format_datetime(errata_detail[ERRATA_ISSUED])),
+                "updated": none2empty(format_datetime(errata_detail[ERRATA_UPDATED])),
+                "cve_list": errata_detail[ERRATA_CVE],
+                "package_list": self.pkgidlist2packages(errata_detail[ERRATA_PKGIDS]),
+                "bugzilla_list": errata_detail[ERRATA_BUGZILLA],
+                "reference_list": errata_detail[ERRATA_REFERENCE],
+                "url": none2empty(errata_detail[ERRATA_URL])
+                }
+        response["errata_list"] = errata_list
         return response
