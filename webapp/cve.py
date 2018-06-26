fe73b03fa93926aa241f4a93f02661d3f1124a21
Gennadii Altukhov <grinrag@gmail.com>
2018-06-26 11:19:17 +0200

    fix #344: compare only non-empty dates with 'modified_since'.

--- webapp/cve.py
+++ webapp/cve.py
@@ -70,8 +70,12 @@ class CveAPI(object):
             cve_detail = self.cache.cve_detail.get(cve, None)
             if not cve_detail:
                 continue
-            if modified_since and (cve_detail[CVE_MODIFIED_DATE] < modified_since_dt
-                                   and cve_detail[CVE_PUBLISHED_DATE] < modified_since_dt):
+
+            # if we have information about modified/published dates and receive "modified_since" in request,
+            # compare the dates
+            if modified_since and \
+                    (cve_detail[CVE_MODIFIED_DATE] and cve_detail[CVE_MODIFIED_DATE] < modified_since_dt or
+                     cve_detail[CVE_PUBLISHED_DATE] and cve_detail[CVE_PUBLISHED_DATE] < modified_since_dt):
                 continue
 
             cve_list[cve] = {
