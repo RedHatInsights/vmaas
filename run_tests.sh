acb63357e3e312312227060552d706c6b61a9223
Patrik Segedy <psegedy@redhat.com>
2018-10-19 16:44:17 +0300

    Run tests using pytest & collect coverage

--- run_tests.sh
+++ run_tests.sh
@@ -15,28 +15,23 @@ rc=0
 # Check for python test files with invalid names
 test_dirs=$(find . -type d -name 'test')
 for test_dir in $test_dirs; do
-    test_files=$(find $test_dir -name '*.py' | grep -v "__init__")
+    test_files=$(find $test_dir -name '*.py' | grep -vE "__init__|schemas|tools|webapp_test_case|yaml_cache")
     for test_file in $test_files; do
         base_filename=$(basename $test_file)
         [[ ! "$base_filename" =~ ^test_.* ]] && echo "ERROR: Invalid test file name - $test_file" && rc=$(($rc+1))
     done
 done
 
-if [ "$TRAVIS_PYTHON_VERSION" != "" ]; then
-    # Use always "python" executable for all Python versions in Travis
-    py_cmd="python"
-elif [ -f /usr/bin/python3 ]; then
-    py_cmd="python3"
-else
-    py_cmd="python"
-fi
 # Find and run tests
-$py_cmd -m unittest discover -v
+pytest --cov=. -v
 rc=$(($rc+$?))
 
 # Run pylint
 find . -iname '*.py' | xargs pylint --rcfile=../pylintrc
 rc=$(($rc+$?))
+
+# Upload to Codecov.io
+codecov
 
 exit $rc
 )
