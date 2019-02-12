3d47790ed3d96ca54a8fca36c9e56e25b3ad7236
Tomas Kasparek <tkasparek@redhat.com>
2019-02-12 11:05:10 +0100

    store module context
    modules are distinguished by N:S:V:C

--- reposcan/repodata/modules.py
+++ reposcan/repodata/modules.py
@@ -25,6 +25,7 @@ class ModuleMD:
                 module_dict[name][stream]['name'] = name
                 module_dict[name][stream]['arch'] = data.get('arch')
                 module_dict[name][stream]['version'] = int(data.get('version'))
+                module_dict[name][stream]['context'] = str(data.get('context'))
                 module_dict[name][stream]['stream'] = str(stream)
                 module_dict[name][stream]['profiles'] = data.get('profiles', [])
                 for profile_name in module_dict[name][stream]['profiles']:
