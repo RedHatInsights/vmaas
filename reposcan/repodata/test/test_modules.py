00cd209685c7afab0e3eb2342817b2663bebc347
Josef Hak <jhak@redhat.com>
2019-05-07 13:46:49 +0200

    renamed var "m" to "mod" because of pylint rules

--- reposcan/repodata/test/test_modules.py
+++ reposcan/repodata/test/test_modules.py
@@ -10,6 +10,6 @@ class TestModuleMD(unittest.TestCase):
 
     def test_modules_loading(self):
         """Test modules.yaml loading and parsing."""
-        m = ModuleMD("test_data/repodata/modules.yaml")
-        self.assertEqual(2, len(m.modules))
-        self.assertEqual(8, len(m.modules[0]))
+        mod = ModuleMD("test_data/repodata/modules.yaml")
+        self.assertEqual(2, len(mod.modules))
+        self.assertEqual(8, len(mod.modules[0]))
