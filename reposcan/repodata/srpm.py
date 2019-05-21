98b1e515a23a6c5b1a4db8cccbdd9ca5867fa312
Josef Hak <jhak@redhat.com>
2019-05-21 14:35:45 +0200

    added rpm name parsing using regex

--- reposcan/repodata/srpm.py
+++ reposcan/repodata/srpm.py
@@ -1,33 +1,19 @@
 """SRPM processing functions module."""
 
+import re
 
-# src: https://github.com/rpm-software-management/yum/blob/master/rpmUtils/miscutils.py
-def parse_srpm_name(filename):
-    """
-    Pass in a standard style rpm fullname
 
-    Return a name, version, release, epoch, arch, e.g.::
-        foo-1.0-1.i386.rpm returns foo, 1.0, 1, i386
-        1:bar-9-123a.ia64.rpm returns bar, 9, 123a, 1, ia64
-    """
+class RPMParseException(Exception):
+    """SRPM name parsing exception."""
 
-    if filename[-4:] == '.rpm':
-        filename = filename[:-4]
 
-    arch_index = filename.rfind('.')
-    arch = filename[arch_index + 1:]
-
-    rel_index = filename[:arch_index].rfind('-')
-    rel = filename[rel_index + 1:arch_index]
-
-    ver_index = filename[:rel_index].rfind('-')
-    ver = filename[ver_index + 1:rel_index]
-
-    epoch_index = filename.find(':')
-    if epoch_index == -1:
+def parse_rpm_name(name):
+    """Extract components from rpm name."""
+    parts = re.search(r'((.*):)?(.*)-(.*)-(.*)\.(.*)\.rpm', name)
+    if parts is None:
+        raise RPMParseException("Failed to parse srpm name!")
+    grps = parts.groups()
+    _, epoch, name, ver, rel, arch = grps
+    if epoch is None:
         epoch = "0"
-    else:
-        epoch = filename[:epoch_index]
-
-    name = filename[epoch_index + 1:ver_index]
     return name, epoch, ver, rel, arch
--- reposcan/repodata/test/test_rpm.py
+++ reposcan/repodata/test/test_rpm.py
@@ -11,7 +11,7 @@ class TestSrpm(unittest.TestCase):
 
     def test_parse_1_srpm(self):
         """Test parsing valid srpm name."""
-        name, epoch, ver, rel, arch = srpm.parse_srpm_name('389-ds-base-1.3.7.8-1.fc27.src.rpm')
+        name, epoch, ver, rel, arch = srpm.parse_rpm_name('389-ds-base-1.3.7.8-1.fc27.src.rpm')
         self.assertEqual("0", epoch)
         self.assertEqual("389-ds-base", name)
         self.assertEqual("1.3.7.8", ver)
@@ -20,7 +20,7 @@ class TestSrpm(unittest.TestCase):
 
     def test_parse_2_rpm(self):
         """Test parsing valid rpm name."""
-        name, epoch, ver, rel, arch = srpm.parse_srpm_name('Agda-2.5.2-9.fc27.x86_64.rpm')
+        name, epoch, ver, rel, arch = srpm.parse_rpm_name('Agda-2.5.2-9.fc27.x86_64.rpm')
         self.assertEqual("0", epoch)
         self.assertEqual("Agda", name)
         self.assertEqual("2.5.2", ver)
@@ -28,10 +28,19 @@ class TestSrpm(unittest.TestCase):
         self.assertEqual("x86_64", arch)
 
     def test_parse_3_epoch(self):
-        """Test parsing valid rpm name."""
-        name, epoch, ver, rel, arch = srpm.parse_srpm_name('3:Agda-2.5.2-9.fc27.x86_64.rpm')
+        """Test parsing valid rpm name with epoch."""
+        name, epoch, ver, rel, arch = srpm.parse_rpm_name('3:Agda-2.5.2-9.fc27.x86_64.rpm')
         self.assertEqual("3", epoch)
         self.assertEqual("Agda", name)
         self.assertEqual("2.5.2", ver)
         self.assertEqual("9.fc27", rel)
         self.assertEqual("x86_64", arch)
+
+    def test_parse_4_invalid_rpmname(self):
+        """Test parsing invalid rpm name."""
+        with self.assertRaises(srpm.RPMParseException):
+            srpm.parse_rpm_name('foo')
+        with self.assertRaises(srpm.RPMParseException):
+            srpm.parse_rpm_name('foo.rpm')
+        with self.assertRaises(srpm.RPMParseException):
+            srpm.parse_rpm_name('foo-1.3.x86.rpm')
