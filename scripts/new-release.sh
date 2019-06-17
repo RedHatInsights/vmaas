643ba1d242865c7aa3308f124ac37adc3faaa781
Josef Hak <jhak@redhat.com>
2019-06-17 11:42:00 +0200

    upated scripts/new-release.sh script
    updated dockerfiles paths
    extended help message
    extended updating log messages

--- scripts/new-release.sh
+++ scripts/new-release.sh
@@ -7,7 +7,7 @@ VERSION_REGEXP="[0-9]+\.[0-9]+"
 RELEASE_BRANCH_PREFIX="vmaas-"
 DOCKER_COMPOSE_FILE="docker-compose.yml"
 TRAVIS_FILE=".travis.yml"
-PATCH_DOCKERFILES="Dockerfile-webapp Dockerfile-reposcan"
+PATCH_DOCKERFILES="*/Dockerfile */Dockerfile.rhel7"
 ENV_FILES="conf/webapp.env conf/reposcan.env"
 
 if [ "$(git rev-parse --abbrev-ref HEAD)" != "$ORIGINAL_BRANCH" ]; then
@@ -31,14 +31,17 @@ if [ "$1" == "-u" ]; then
 elif [[ "$1" =~ $VERSION_REGEXP ]]; then
     echo "Creating new tag: v$1"
     git tag "v$1"
+
     echo "Creating new branch: $RELEASE_BRANCH_PREFIX$1"
     git checkout -b "$RELEASE_BRANCH_PREFIX$1"
-    echo "Updating $DOCKER_COMPOSE_FILE..."
+    echo " Updating $DOCKER_COMPOSE_FILE..."
     sed -i "s/:latest/:$1/g" "$DOCKER_COMPOSE_FILE"
+    echo " Updating $TRAVIS_FILE..."
     sed -i "s/- master/- $RELEASE_BRANCH_PREFIX$1/g" "$TRAVIS_FILE"
-    for dockerfile in $PATCH_DOCKERFILES; do
-        sed -i "s/ENV VMAAS_VERSION=latest/ENV VMAAS_VERSION=$1/g" "$dockerfile"
-        git add "$dockerfile"
+    for DOCKERFILE in $PATCH_DOCKERFILES; do
+        echo " Updating $DOCKERFILE..."
+        sed -i "s/ENV VMAAS_VERSION=latest/ENV VMAAS_VERSION=$1/g" "$DOCKERFILE"
+        git add "$DOCKERFILE"
     done
     git add "$DOCKER_COMPOSE_FILE" "$TRAVIS_FILE"
     git commit -m "Update version to $1"
@@ -51,7 +54,13 @@ else
 fi
 
 if [ $print_help -eq 1 ]; then
-    echo "This script creates a new release branch from master."
+    echo "This script creates a new release stuffs:"
+    echo "- new tag 'vVERSION'"
+    echo "- new branch 'vmaas-VERSION' from master updating:"
+    echo "  - VMAAS_VERSION in dockerfiles"
+    echo "  - image names in docker-compose.yml"
+    echo "  - branch param in .travis.yml"
+    echo ""
     echo "Usage:"
     echo "    $0 version"
     echo "    (version matching \"$VERSION_REGEXP\")"
@@ -61,4 +70,3 @@ if [ $print_help -eq 1 ]; then
     echo "    $0 -u 0.1"
 fi
 )
-
