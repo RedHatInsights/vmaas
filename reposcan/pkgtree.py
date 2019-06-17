0bdc8b90c755f938ee0e102ce4617937ec6a56d0
Eric Herget <eherget@redhat.com>
2019-06-17 18:57:28 -0400

    add modules to pkgtree output

--- reposcan/pkgtree.py
+++ reposcan/pkgtree.py
@@ -40,6 +40,9 @@ class JsonPkgTree: # pylint: disable=too-many-instance-attributes
         self.repodata = {}
         self.cvename = {}
         self.erratadata = {}
+        self.streamid2stream = {}
+        self.moduleid2module = {}
+        self.pkgrepomodules = {}
         self.pkgtree_indent = int(os.getenv('PKGTREE_INDENT', DEFAULT_PKGTREE_INDENT))
         self.pkgtree_keep_copies = int(os.getenv('PKGTREE_KEEP_COPIES', DEFAULT_KEEP_COPIES))
 
@@ -57,16 +60,19 @@ class JsonPkgTree: # pylint: disable=too-many-instance-attributes
         self.outputdata['timestamp'] = timestamp
         self.outputdata['packages'] = {}
         LOGGER.info("Loading data")
-        self.load_packagenames()
-        self.load_evr()
-        self.load_arch()
-        self.load_repodata()
-        self.load_cves()
-        self.load_errata()
-        self.associate_cves_to_errata()
-        self.load_packages()
-        self.associate_repos()
-        self.associate_errata()
+        self._load_packagenames()
+        self._load_evr()
+        self._load_arch()
+        self._load_repodata()
+        self._load_cves()
+        self._load_errata()
+        self._associate_cves_to_errata()
+        self._load_packages()
+        self._load_module_streams()
+        self._load_modules()
+        self._associate_modules()
+        self._associate_repos()
+        self._associate_errata()
         LOGGER.info("Exporting data to %s", dump_filename)
         with gzip.open(dump_filename, 'wt') as dump_file:
             json.dump(self.outputdata, dump_file, indent=self.pkgtree_indent, ensure_ascii=False)
@@ -83,7 +89,7 @@ class JsonPkgTree: # pylint: disable=too-many-instance-attributes
             LOGGER.info("Removing old dump %s", fname)
             os.unlink(fname)
 
-    def load_packagenames(self):
+    def _load_packagenames(self):
         """Load the datadict and start filling in outputdata"""
         with self._named_cursor() as cursor:
             cursor.execute("""select id, name
@@ -94,21 +100,21 @@ class JsonPkgTree: # pylint: disable=too-many-instance-attributes
                 self.pkgnameid2pkgname[name_id] = pkg_name
                 self.outputdata['packages'][pkg_name] = self.datadict[name_id]
 
-    def load_evr(self):
+    def _load_evr(self):
         """Load the evrid2evr dict"""
         with self._named_cursor() as cursor:
             cursor.execute("select id, epoch, version, release from evr")
             for evr_id, epoch, ver, rel in cursor:
                 self.evrid2evr[evr_id] = (epoch, ver, rel)
 
-    def load_arch(self):
+    def _load_arch(self):
         """Load the archid2arch dict"""
         with self._named_cursor() as cursor:
             cursor.execute("select id, name from arch")
             for arch_id, name in cursor:
                 self.archid2arch[arch_id] = name
 
-    def load_repodata(self):
+    def _load_repodata(self):
         """Load the repo data"""
         with self._named_cursor() as cursor:
             cursor.execute("""select r.id,
@@ -132,14 +138,14 @@ class JsonPkgTree: # pylint: disable=too-many-instance-attributes
                                                    'revision':format_datetime(revision)}
                                          }
 
-    def load_cves(self):
+    def _load_cves(self):
         """Load CVE data"""
         with self._named_cursor() as cursor:
             cursor.execute("select id, name from cve")
             for cve_id, name in cursor:
                 self.cvename[cve_id] = name
 
-    def load_errata(self):
+    def _load_errata(self):
         """Load the errata data"""
         with self._named_cursor() as cursor:
             cursor.execute("select id, name, issued from errata")
@@ -149,14 +155,14 @@ class JsonPkgTree: # pylint: disable=too-many-instance-attributes
                                                        'issued': format_datetime(issued)}
                                              }
 
-    def associate_cves_to_errata(self):
+    def _associate_cves_to_errata(self):
         """Associate CVEs to errata"""
         with self._named_cursor() as cursor:
             cursor.execute("select errata_id, cve_id from errata_cve")
             for errata_id, cve_id in cursor:
                 self.erratadata[errata_id]['data'].setdefault('cve_list', []).append(self.cvename[cve_id])
 
-    def load_packages(self):
+    def _load_packages(self):
         """Load the packages info"""
         with self._named_cursor() as cursor:
             cursor.execute("""select p.id, p.name_id, p.evr_id, p.arch_id
@@ -164,10 +170,9 @@ class JsonPkgTree: # pylint: disable=too-many-instance-attributes
                           inner join evr on p.evr_id = evr.id
                             order by evr.evr""")
             for pkg_id, name_id, evr_id, arch_id in cursor:
-                self.add_package_entry(pkg_id, name_id, evr_id, arch_id)
+                self._add_package_entry(pkg_id, name_id, evr_id, arch_id)
 
-
-    def add_package_entry(self, pkg_id, name_id, evr_id, arch_id):
+    def _add_package_entry(self, pkg_id, name_id, evr_id, arch_id):
         """Add package to the ultimate response"""
         if name_id not in self.pkgnameid2pkgname:
             LOGGER.error("INCONSISTENT DATA: pkg id %s - package name id %s not found", pkg_id, name_id)
@@ -182,14 +187,26 @@ class JsonPkgTree: # pylint: disable=too-many-instance-attributes
                                    }
         self.datadict[name_id].append(self.packagedata[pkg_id]['data'])
 
-    def associate_repos(self):
+    def _associate_repos(self):
         """Add repodata to the package entries"""
         with self._named_cursor() as cursor:
             cursor.execute("select pkg_id, repo_id from pkg_repo")
             for pkg_id, repo_id in cursor:
-                self.packagedata[pkg_id]['data']['repositories'].append(self.repodata[repo_id]['data'])
+                if (pkg_id, repo_id) in self.pkgrepomodules:
+                    # create new dict and copy data if we need to add module data
+                    addrepo = {'label': self.repodata[repo_id]['data']['label'],
+                               'name': self.repodata[repo_id]['data']['name'],
+                               'arch': self.repodata[repo_id]['data']['arch'],
+                               'releasever': self.repodata[repo_id]['data']['releasever'],
+                               'revision': self.repodata[repo_id]['data']['revision'],
+                               'module_name': self.pkgrepomodules[(pkg_id, repo_id)]['name'],
+                               'module_stream': self.pkgrepomodules[(pkg_id, repo_id)]['stream']}
+                    self.packagedata[pkg_id]['data']['repositories'].append(addrepo)
+                else:
+                    # otherwise save memory
+                    self.packagedata[pkg_id]['data']['repositories'].append(self.repodata[repo_id]['data'])
 
-    def associate_errata(self):
+    def _associate_errata(self):
         """Add errata data to the package entries"""
         with self._named_cursor() as cursor:
             cursor.execute("select pkg_id, errata_id from pkg_errata")
@@ -201,6 +218,36 @@ class JsonPkgTree: # pylint: disable=too-many-instance-attributes
                         self.packagedata[pkg_id]['first_published'] = edata['issued']
                         self.packagedata[pkg_id]['data']['first_published'] = edata['data']['issued']
                 self.packagedata[pkg_id]['data'].setdefault('errata', []).append(edata['data'])
+
+    def _load_module_streams(self):
+        """Load the module streams info"""
+        with self._named_cursor() as cursor:
+            cursor.execute("select ms.id, ms.module_id, ms.stream_name from module_stream ms")
+            for module_stream_id, module_id, stream_name in cursor:
+                self.streamid2stream[module_stream_id] = {'stream_name': stream_name,
+                                                          'module_id': module_id}
+
+    def _load_modules(self):
+        """Load the modules info"""
+        with self._named_cursor() as cursor:
+            cursor.execute("select m.id, m.name, m.repo_id from module m")
+            for module_id, name, repo_id in cursor:
+                self.moduleid2module[module_id] = {'name': name,
+                                                   'repo_id': repo_id}
+
+    def _associate_modules(self):
+        """Add module info to the package repository entries"""
+        with self._named_cursor() as cursor:
+            cursor.execute("select pkg_id, stream_id from module_rpm_artifact")
+            for pkg_id, stream_id in cursor:
+                stream = self.streamid2stream[stream_id]['stream_name']
+                module_id = self.streamid2stream[stream_id]['module_id']
+                module = self.moduleid2module[module_id]['name']
+                repo_id = self.moduleid2module[module_id]['repo_id']
+                if (pkg_id, repo_id) in self.pkgrepomodules:
+                    LOGGER.warning('found multiple modules for pkg_id, repo_id = %s, %s', pkg_id, repo_id)
+                self.pkgrepomodules[(pkg_id, repo_id)] = {'name': module,
+                                                          'stream': stream}
 
 
 def main(filename):
