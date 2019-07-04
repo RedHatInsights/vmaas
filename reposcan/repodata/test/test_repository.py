9d9e2e582ff75a141c8e5f57fe8d0158ea807d0d
Josef Hak <jhak@redhat.com>
2019-07-04 16:07:56 +0200

    added modules loading test

--- reposcan/repodata/test/test_repository.py
+++ reposcan/repodata/test/test_repository.py
@@ -5,21 +5,29 @@ import unittest
 from repodata.primary import PrimaryMD
 from repodata.primary_db import PrimaryDatabaseMD
 from repodata.updateinfo import UpdateInfoMD
+from repodata.modules import ModuleMD
 from repodata.repository import Repository
 from repodata.test.test_updateinfo import KNOWN_UPDATE_TYPES
+
+PRIMARY_SQLITE_PATH = "test_data/repodata/primary_db.sqlite"
+PRIMARY_XML_PATH = "test_data/repodata/primary.xml"
+UPDATEINFO_XML_PATH = "test_data/repodata/updateinfo.xml"
+MODULES_YAML_PATH = "test_data/repodata/modules.yaml"
 
 
 class TestRepository(unittest.TestCase):
     """Test Repository class."""
     def setUp(self):
         """Setup example files."""
-        primary_db = PrimaryDatabaseMD("test_data/repodata/primary_db.sqlite")
-        primary = PrimaryMD("test_data/repodata/primary.xml")
-        updateinfo = UpdateInfoMD("test_data/repodata/updateinfo.xml")
+        primary_db = PrimaryDatabaseMD(PRIMARY_SQLITE_PATH)
+        primary = PrimaryMD(PRIMARY_XML_PATH)
+        updateinfo = UpdateInfoMD(UPDATEINFO_XML_PATH)
+        modules = ModuleMD(MODULES_YAML_PATH)
 
         self.repository = Repository("repo_url", "content_set", "x86_64", "27")
         self.repository.primary = primary_db
         self.repository.updateinfo = updateinfo
+        self.repository.modules = modules
         self.repository_without_updateinfo = Repository("repo_url", "content_set", "x86_64", "27")
         self.repository_without_updateinfo.primary = primary_db
         self.repository_primary_xml = Repository("repo_url", "content_set", "x86_64", "27")
@@ -51,5 +59,29 @@ class TestRepository(unittest.TestCase):
 
     def test_listing(self):
         """Test package and update methods."""
-        self.assertEqual(len(self.repository.list_packages()), self.repository.get_package_count())
-        self.assertEqual(len(self.repository.list_updates()), self.repository.get_update_count())
+        self.assertEqual(12, len(self.repository.list_packages()))
+        self.assertEqual(12, self.repository.get_package_count())
+
+        self.assertEqual(9, len(self.repository.list_updates()))
+        self.assertEqual(9, self.repository.get_update_count())
+
+        self.assertEqual(2, len(self.repository.list_modules()))
+
+    def test_load_metadata(self):
+        """Test package and update methods."""
+        repo = Repository(repo_url='', content_set='', basearch='', releasever='')
+        self.assertEqual(repo.md_files, {})
+        self.assertIsNone(repo.primary)
+        self.assertIsNone(repo.updateinfo)
+        self.assertIsNone(repo.modules)
+        repo.md_files = dict(primary_db=PRIMARY_SQLITE_PATH, primary=PRIMARY_XML_PATH, updateinfo=UPDATEINFO_XML_PATH,
+                             modules=MODULES_YAML_PATH)
+        repo.load_metadata()
+        self.assertIsNotNone(repo.primary)
+        self.assertIsNotNone(repo.updateinfo)
+        self.assertIsNotNone(repo.modules)
+
+        repo.unload_metadata()
+        self.assertIsNone(repo.primary)
+        self.assertIsNone(repo.updateinfo)
+        self.assertIsNone(repo.modules)
