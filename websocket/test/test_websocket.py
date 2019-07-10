361f9a48440d598ffa3b4f3a0439e561fe498487
Josef Hak <jhak@redhat.com>
2019-07-10 19:58:55 +0200

    added websocket tornado test

---
+++ websocket/test/test_websocket.py
@@ -0,0 +1,20 @@
+"""Test VMaaS websocket."""
+import http
+
+import tornado.testing
+import tornado.web
+
+from websocket import websocket as ws
+
+
+class TestWebSocket(tornado.testing.AsyncHTTPTestCase):
+    """Test VMaaS websocket."""
+
+    def get_app(self):
+        app = ws.WebsocketApplication()
+        return app
+
+    def test_monitoring_health(self):
+        """Test reposcan invalidate cache notification."""
+        resp = self.fetch("/api/v1/monitoring/health/", method="GET")
+        self.assertEqual(resp.code, http.HTTPStatus.OK)
