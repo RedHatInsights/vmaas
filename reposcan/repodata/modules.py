58fe38d52789aa34d7a7035a1e014bc41cea5c20
Eric Herget <eherget@redhat.com>
2019-07-15 17:22:53 -0400

    fix module parsing

--- reposcan/repodata/modules.py
+++ reposcan/repodata/modules.py
@@ -22,24 +22,27 @@ class ModuleMD:
                 stream = data['stream']
                 if not name in module_dict:
                     module_dict[name] = {}
-                module_dict[name][stream] = {}
-                module_dict[name][stream]['name'] = name
-                module_dict[name][stream]['arch'] = data.get('arch')
-                module_dict[name][stream]['version'] = int(data.get('version'))
-                module_dict[name][stream]['context'] = str(data.get('context'))
-                module_dict[name][stream]['stream'] = str(stream)
-                module_dict[name][stream]['profiles'] = data.get('profiles', [])
-                for profile_name in module_dict[name][stream]['profiles']:
-                    module_dict[name][stream]['profiles'][profile_name]['default_profile'] = False
+                if not stream in module_dict[name]:
+                    module_dict[name][stream] = []
+                new_stream = {}
+                new_stream['name'] = name
+                new_stream['arch'] = data.get('arch')
+                new_stream['version'] = int(data.get('version'))
+                new_stream['context'] = str(data.get('context'))
+                new_stream['stream'] = str(stream)
+                new_stream['profiles'] = data.get('profiles', [])
+                for profile_name in new_stream['profiles']:
+                    new_stream['profiles'][profile_name]['default_profile'] = False
                 if 'artifacts' in data:
-                    module_dict[name][stream]['artifacts'] = data.get('artifacts').get('rpms', [])
-                module_dict[name][stream]['default_stream'] = False
+                    new_stream['artifacts'] = data.get('artifacts').get('rpms', [])
+                new_stream['default_stream'] = False
+                module_dict[name][stream].append(new_stream)
             elif parsed['document'] == 'modulemd-defaults':
                 default_list.append(parsed)
         self._parse_modulemd_defaults(module_dict, default_list)
         for name in module_dict:
             for stream in module_dict[name]:
-                self.modules.append(module_dict[name][stream])
+                self.modules.extend(module_dict[name][stream])
 
     @staticmethod
     def _parse_modulemd_defaults(module_dict, default_list):
@@ -47,14 +50,17 @@ class ModuleMD:
             data = parsed['data']
             name = data.get('module')
             default_stream = data.get('stream')
-            if default_stream:
-                module_dict[name][default_stream]['default_stream'] = True
+            if default_stream and default_stream in module_dict[name]:
+                for stream_dict in module_dict[name][default_stream]:
+                    stream_dict['default_stream'] = True
             for stream in data['profiles']:
                 if stream not in module_dict[name]:
                     continue  # this is here due to module defaults referencing non-existing modules
                 default_profile = data['profiles'][stream]
-                if default_profile and default_profile[0] in module_dict[name][stream]['profiles']:
-                    module_dict[name][stream]['profiles'][default_profile[0]]['default_profile'] = True
+                if default_profile:
+                    for stream_dict in module_dict[name][stream]:
+                        if default_profile[0] and default_profile[0] in stream_dict['profiles']:
+                            stream_dict['profiles'][default_profile[0]]['default_profile'] = True
 
     def list_modules(self):
         """Returns list of parsed modules (list of dictionaries)."""
