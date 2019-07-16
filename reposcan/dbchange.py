05871c58a152f4f82527e5267e03e176cf480576
Tomáš Sasák <tsasak@redhat.com>
2019-07-16 16:02:53 +0200

    Add pkgtree_change item in dbchange get API.

---
+++ reposcan/dbchange.py
@@ -0,0 +1,22 @@
+"""
+Dbchange API implementation.
+"""
+from database.database_handler import DatabaseHandler, init_db
+
+
+class DbChangeAPI:
+    """API class to work with dbchange. """
+    db_instance = None
+
+    def process(self):
+        """Processes the dbchange get request. """
+        init_db()
+        self.db_instance = DatabaseHandler.get_connection()
+        result = {}
+
+        with self.db_instance.cursor() as crs:
+            crs.execute("select pkgtree_change from dbchange")
+            timestamp = crs.fetchone()
+
+        result["pkgtree_change"] = str(timestamp[0])
+        return result
