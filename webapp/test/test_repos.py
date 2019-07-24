bbdcef99bab948e388196ab1ce2cfe582fcd17e0
Andrii But <abut@redhat.com>
2019-07-24 14:20:16 +0200

    Add tests

--- webapp/test/test_repos.py
+++ webapp/test/test_repos.py
@@ -1,5 +1,6 @@
 """Unit test for CveAPI module."""
-from test import schemas
+
+from test import schemas, tools
 from test.conftest import TestBase
 
 import pytest
@@ -9,6 +10,10 @@ from repos import RepoAPI
 REPO_JSON_EMPTY = {}
 REPO_JSON_BAD = {"page_size": 9}
 REPO_JSON = {"repository_list": ["rhel-7-server-rpms"]}
+REPO_JSON_MODIFIED_SINCE = {
+    "repository_list": ["rhel-7-server-rpms"],
+    "modified_since": "2099-01-01T00:00:00+00:00"
+}
 REPO_JSON_EMPTY_LIST = {"repository_list": [""]}
 REPO_JSON_NON_EXIST = {"repository_list": ["non-existent-repo"]}
 
@@ -55,7 +60,7 @@ class TestRepoAPI(TestBase):
         assert response == EMPTY_RESPONSE
 
     def test_non_existing_repo(self):
-        """Test repos API repsonse for non-existent repo."""
+        """Test repos API response for non-existent repo."""
         response = self.repo.process_list(api_version="v1", data=REPO_JSON_NON_EXIST)
         assert response == EMPTY_RESPONSE
 
@@ -63,3 +68,24 @@ class TestRepoAPI(TestBase):
         """Test schema of valid repos API response."""
         response = self.repo.process_list(api_version="v1", data=REPO_JSON)
         assert schemas.repos_schema.validate(response)
+
+    def test_modified_since(self):
+        """Test repos API with 'modified_since' property."""
+        response = self.repo.process_list(api_version=1, data=REPO_JSON_MODIFIED_SINCE)
+        assert tools.match(EMPTY_RESPONSE, response) is True
+
+    def test_page_size(self):
+        """Test repos API page size"""
+        response = self.repo.process_list(api_version="v1", data=REPO_JSON)
+        page_size = 0
+        for label in response['repository_list']:
+            page_size += len(response['repository_list'][label])
+        assert response['page_size'] == page_size
+
+    def test_page_size_mod_since(self):
+        """Test repos API page size with 'modified_since' property"""
+        response = self.repo.process_list(api_version="v1", data=REPO_JSON_MODIFIED_SINCE)
+        page_size = 0
+        for label in response['repository_list']:
+            page_size += len(response['repository_list'][label])
+        assert response['page_size'] == page_size
