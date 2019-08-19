74c014bce61ef1721443aa4c00041f8b199cf4d9
Tomáš Sasák <tsasak@redhat.com>
2019-08-19 15:55:55 +0200

    Add webapp_utils packages API tests.

--- webapp_utils/default_handler.py
+++ webapp_utils/default_handler.py
@@ -5,7 +5,7 @@ Module for /health API endpoint
 from base import Request
 
 
-class GetHealth(Request):
+class GetHealth(Request): # pylint: disable=abstract-method
     """GET to /v1/health"""
     _endpoint_name = r'/v1/health'
 
