c8e5f8488d39346b3690471f14027091682cb657
Jan Dobes <jdobes@redhat.com>
2019-08-27 13:25:30 +0200

    include only relevant RPMs in manifest

--- scripts/generate_manifest.sh
+++ scripts/generate_manifest.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
-# ./scripts/generate_manifest.sh <MANIFEST_PATH> <PREFIX> <PYTHON-CMD-OPTIONAL>
+# ./scripts/generate_manifest.sh <MANIFEST_PATH> <PREFIX> <BASE_RPM_LIST> <FINAL_RPM_LIST> <PYTHON-CMD-OPTIONAL>
 # Example:
-# ./scripts/generate_manifest.sh manifest_webapp.txt my-service python
+# ./scripts/generate_manifest.sh manifest_webapp.txt my-service /tmp/base_rpm_list.txt /tmp/final_rpm_list.txt python
 # cat manifest_webapp.txt
 
 MANIFEST_PATH=$1
 PREFIX=$2
-PYTHON=$3
+BASE_RPM_LIST=$3
+FINAL_RPM_LIST=$4
+PYTHON=$5
 
-## Write rpm packages.
-rpm -qa --qf='%{sourcerpm}\n' | grep -v '(none)' | sort -u | sed 's/\.src\.rpm$//' > ${MANIFEST_PATH}
+grep -v -f ${BASE_RPM_LIST} ${FINAL_RPM_LIST} > ${MANIFEST_PATH}
 
 ## Write Python packages if python set.
 if [[ ! -z $PYTHON ]]
