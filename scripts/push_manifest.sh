24a2b067855925ff39e07148b173e13ec62a1bc0
Jan Dobes <jdobes@redhat.com>
2019-08-28 12:21:44 +0200

    change push script to commit using github API

--- scripts/push_manifest.sh
+++ scripts/push_manifest.sh
@@ -1,37 +1,42 @@
 #!/bin/sh
 
-# GIT_TOKEN=<GIT_TOKEN> ./scripts/push_manifest.sh <GIT_REMOTE> <GIT_BRANCH> <GIT_USER> <SOURCE_FILE_PATH> <GIT_FILE_PATH>
+# GIT_TOKEN=<GIT_TOKEN> ./scripts/push_manifest.sh <GIT_REPO> <GIT_BRANCH> <SOURCE_FILE_PATH> <GIT_FILE_PATH>
 # Example:
-# GIT_TOKEN=mygithubtoken ./scripts/push_manifest.sh https://github.com/RedHatInsights/manifests.git master vulnerability-insights-bot /manifest.txt vulnerability-engine/vulnerability-engine-manager.txt
+# GIT_TOKEN=mygithubtoken ./scripts/push_manifest.sh RedHatInsights/manifests master /manifest.txt vulnerability-engine/vulnerability-engine-manager.txt
 
-GIT_REMOTE=$1
+GIT_REPO=$1
 GIT_BRANCH=$2
-GIT_USER=$3
-SOURCE_FILE_PATH=$4
-GIT_FILE_PATH=$5
+SOURCE_FILE_PATH=$3
+GIT_FILE_PATH=$4
+
+API_ENDPOINT="https://api.github.com/repos/$GIT_REPO/contents/$GIT_FILE_PATH"
 
 if [[ ! -z $GIT_TOKEN ]]
 then
-    (
-        GIT_REMOTE="${GIT_REMOTE/https:\/\//https:\/\/$GIT_USER:$GIT_TOKEN@}"
-        cd /tmp
-        git clone $GIT_REMOTE && cd "$(basename "$GIT_REMOTE" .git)"
-        git config user.email "no-reply@localhost" && git config user.name "Vulnerability Insights Bot"
-        git checkout $GIT_BRANCH
-	mkdir -p $(dirname $GIT_FILE_PATH) && cp $SOURCE_FILE_PATH $GIT_FILE_PATH
-        git add $GIT_FILE_PATH
-        git commit -m "Updating $GIT_FILE_PATH"
-        retry=0
-        until [ $retry -ge 5 ]
-        do
-            git push origin $GIT_BRANCH && break
-            retry=$((retry+1))
-            echo "Push failed, trying to rebase after 1 second..."
-            sleep 1
-            git pull -r origin $GIT_BRANCH
-        done
-        rm -rf $(pwd)
-    )
+    retry=0
+    until [ $retry -ge 5 ]
+    do
+        curl -H "Authorization: token $GIT_TOKEN" -X GET $API_ENDPOINT?ref=$GIT_BRANCH | python -c "import json,sys;a=json.load(sys.stdin);print(a.get('content',''))" | base64 -d | diff $SOURCE_FILE_PATH -
+        diff_rc=$?
+        if [ $diff_rc -eq 0 ]
+        then
+            echo "Remote manifest is already up to date!"
+            break
+        fi
+        # fetch remote file sha (if exists)
+        remote_file_sha=$(curl -H "Authorization: token $GIT_TOKEN" -X GET $API_ENDPOINT?ref=$GIT_BRANCH | python -c "import json,sys;a=json.load(sys.stdin);print(a.get('sha',''))")
+        # insert or update file
+        echo "{\"message\": \"Updating $GIT_FILE_PATH\", \"branch\": \"$GIT_BRANCH\", \"sha\": \"$remote_file_sha\", \"content\": \"$(base64 -w 0 $SOURCE_FILE_PATH)\"}" > /tmp/commit_payload.json
+        new_commit_sha=$(curl -H "Authorization: token $GIT_TOKEN" -X PUT -d "@/tmp/commit_payload.json" $API_ENDPOINT | python -c "import json,sys;a=json.load(sys.stdin);print(a.get('commit',{}).get('sha',''))")
+        # commit sha returned => success
+        if [[ ! -z $new_commit_sha ]]
+        then
+            break
+        fi
+        retry=$((retry+1))
+        echo "Update failed, trying again after 1 second..."
+        sleep 1
+    done
 else
     echo "GIT_TOKEN is not set, not pushing anything."
 fi
