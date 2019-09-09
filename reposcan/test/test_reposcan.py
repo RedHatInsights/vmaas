0df4074f132c971830ea6622188ee0b4692e7ce6
Michal Hornický <mhornick@redhat.com>
2019-09-09 11:20:09 +0200

    Re-implement basic API endpoint tests
    These utilized tornado.testing.AsyncHTTPTestCase. This was replaced by base_case.py,
    which provides pytest fixtures, and utility methods.
    
    Fix security scheme to accept github formatted Authorization: token <token> header
    
    Remove unnecessary base.py, and clean up the reposcan code

--- reposcan/test/test_reposcan.py
+++ reposcan/test/test_reposcan.py
@@ -3,33 +3,21 @@
 import os
 
 from http import HTTPStatus
-
-from tornado.testing import AsyncHTTPTestCase
-
-import reposcan
+from .test_case import FlaskTestCase
 
 
-class TestReposcanApp(AsyncHTTPTestCase):
+class TestReposcanApp(FlaskTestCase):
     """Reposcan overall test."""
-
-    def get_app(self):
-        """Setup testing app."""
-        return reposcan.ReposcanApplication()
 
     def test_monitoring_health(self):
         """Test monitoring health endpoint."""
         resp = self.fetch('/api/v1/monitoring/health', method='GET')
-        self.assertEqual(HTTPStatus.OK, resp.code)
-
-    def test_apispec(self):
-        """Test apispec endpoint."""
-        resp = self.fetch('/api/v1/apispec', method='GET')
-        self.assertEqual(HTTPStatus.OK, resp.code)
+        self.assertEqual(HTTPStatus.OK, resp.status)
 
     def test_version(self):
         """Test version endpoint."""
         resp = self.fetch('/api/v1/version', method='GET')
-        self.assertEqual(HTTPStatus.OK, resp.code)
+        self.assertEqual(HTTPStatus.OK, resp.status)
 
     def test_add_repo_1(self):
         """Test add repo request."""
@@ -49,10 +37,10 @@ class TestReposcanApp(AsyncHTTPTestCase):
            "entitlement_cert": {"name": "a", "ca_cert": "b", "cert": "c", "key": "d"}
         }]"""
 
-        resp = self.fetch('/api/v1/repos', method='POST', body=body)
-        self.assertEqual(HTTPStatus.OK, resp.code)
-        self.assertEqual(b'{"msg": "Import repositories task started.", "success": true}', resp.body)
-        resp = self.fetch('/api/v1/task/cancel', method='PUT', body=body)
+        resp = self.fetch('/api/v1/repos', method='POST', data=body)
+        self.assertEqual(HTTPStatus.OK, resp.status)
+        self.assertEqual({"msg": "Import repositories task started.", "success": True}, resp.body)
+        resp = self.fetch('/api/v1/task/cancel', method='PUT', data=body)
 
     def test_add_repo_1_with_certs(self):
         """Test_add_repo - with certs and keys"""
@@ -82,71 +70,71 @@ class TestReposcanApp(AsyncHTTPTestCase):
             }
         }]"""
 
-        resp = self.fetch('/api/v1/repos', method='POST', body=body)
-        self.assertEqual(HTTPStatus.OK, resp.code)
-        self.assertEqual(b'{"msg": "Import repositories task started.", "success": true}', resp.body)
-        resp = self.fetch('/api/v1/task/cancel', method='PUT', body=body)
+        resp = self.fetch('/api/v1/repos', method='POST', data=body)
+        self.assertEqual(HTTPStatus.OK, resp.status)
+        self.assertEqual({"msg": "Import repositories task started.", "success": True}, resp.body)
+        resp = self.fetch('/api/v1/task/cancel', method='PUT', data=body)
 
     def test_add_repo_2(self):
         """Test add repo - bad request."""
         body = """[{"wrong_key: {}"}]"""
-        resp = self.fetch('/api/v1/repos', method='POST', body=body)
-        self.assertEqual(HTTPStatus.BAD_REQUEST, resp.code)
+        resp = self.fetch('/api/v1/repos', method='POST', data=body)
+        self.assertEqual(HTTPStatus.BAD_REQUEST, resp.status)
 
     def test_delete_repo(self):
         """Test delete repo endpoint."""
         resp = self.fetch('/api/v1/repos/myrepo', method='DELETE')
-        self.assertTrue(resp.code in [HTTPStatus.OK, HTTPStatus.TOO_MANY_REQUESTS])
+        self.assertTrue(resp.status in [HTTPStatus.OK, HTTPStatus.TOO_MANY_REQUESTS])
 
     def test_sync_all(self):
         """Test sync all endpoint."""
-        resp = self.fetch('/api/v1/sync', method='PUT', body="{}")
-        self.assertTrue(resp.code in [HTTPStatus.OK, HTTPStatus.TOO_MANY_REQUESTS])
+        resp = self.fetch('/api/v1/sync', method='PUT', data="{}")
+        self.assertTrue(resp.status in [HTTPStatus.OK, HTTPStatus.TOO_MANY_REQUESTS])
 
     def test_sync_repo(self):
         """Test sync repo endpoint."""
-        resp = self.fetch('/api/v1/sync/repo', method='PUT', body="{}")
-        self.assertTrue(resp.code in [HTTPStatus.OK, HTTPStatus.TOO_MANY_REQUESTS])
+        resp = self.fetch('/api/v1/sync/repo', method='PUT', data="{}")
+        self.assertTrue(resp.status in [HTTPStatus.OK, HTTPStatus.TOO_MANY_REQUESTS])
 
     def test_sync_cve(self):
         """Test sync cve endpoint."""
-        resp = self.fetch('/api/v1/sync/cve', method='PUT', body="{}")
-        self.assertTrue(resp.code in [HTTPStatus.OK, HTTPStatus.TOO_MANY_REQUESTS])
+        resp = self.fetch('/api/v1/sync/cve', method='PUT', data="{}")
+        self.assertTrue(resp.status in [HTTPStatus.OK, HTTPStatus.TOO_MANY_REQUESTS])
 
     def test_sync_cvemap(self):
         """Test sync cvemap endpoint."""
-        resp = self.fetch('/api/v1/sync/cvemap', method='PUT', body="{}")
-        self.assertTrue(resp.code in [HTTPStatus.OK, HTTPStatus.TOO_MANY_REQUESTS])
+        resp = self.fetch('/api/v1/sync/cvemap', method='PUT', data="{}")
+        self.assertTrue(resp.status in [HTTPStatus.OK, HTTPStatus.TOO_MANY_REQUESTS])
 
     def test_sync_pkgtree(self):
         """Test sync_pkgtree endpoint."""
-        resp = self.fetch('/api/v1/sync/pkgtree', method='PUT', body="{}")
-        self.assertTrue(resp.code in [HTTPStatus.OK, HTTPStatus.TOO_MANY_REQUESTS])
+        resp = self.fetch('/api/v1/sync/pkgtree', method='PUT', data="{}")
+        self.assertTrue(resp.status in [HTTPStatus.OK, HTTPStatus.TOO_MANY_REQUESTS])
 
     def test_export(self):
         """Test export endpoint."""
-        resp = self.fetch('/api/v1/export', method='PUT', body="{}")
-        self.assertTrue(resp.code in [HTTPStatus.OK, HTTPStatus.TOO_MANY_REQUESTS])
+        resp = self.fetch('/api/v1/export', method='PUT', data="{}")
+        self.assertTrue(resp.status in [HTTPStatus.OK, HTTPStatus.TOO_MANY_REQUESTS])
 
     def test_pkgtree(self):
         """Test pkgtree endpoint."""
         resp = self.fetch('/api/v1/pkgtree', method='GET')
-        self.assertEqual(HTTPStatus.NOT_FOUND, resp.code)
-        self.assertEqual("Package Tree file not found.  Has it been generated?", resp.error.message)
+        self.assertEqual(HTTPStatus.NOT_FOUND, resp.status)
+        assert resp.data == b"\"Package Tree file not found.  Has it been generated?\"\n"
+
+    def test_task_cancel(self):
+        """Test cancel task."""
+        resp = self.fetch('/api/v1/task/cancel', method='PUT', data="{}")
+        self.assertEqual(HTTPStatus.OK, resp.status)
+        self.assertEqual({"running": False, "task_type": None}, resp.body)
 
     def test_task_status(self):
         """Test get status request."""
         resp = self.fetch('/api/v1/task/status', method='GET')
-        self.assertEqual(HTTPStatus.OK, resp.code)
-        self.assertEqual(b'{"running": false, "task_type": null}', resp.body)
-
-    def test_task_cancel(self):
-        """Test cancel task."""
-        resp = self.fetch('/api/v1/task/cancel', method='PUT', body="{}")
-        self.assertEqual(HTTPStatus.OK, resp.code)
-        self.assertEqual(b'{"running": false, "task_type": null}', resp.body)
+        self.assertEqual(HTTPStatus.OK, resp.status)
+        self.assertEqual({"running": False, "task_type": None}, resp.body)
 
     def test_metrics(self):
         """Test metrics endpoint."""
         resp = self.fetch('/metrics', method='GET')
-        self.assertEqual(HTTPStatus.OK, resp.code)
+        self.assertEqual(HTTPStatus.OK, resp.status)
