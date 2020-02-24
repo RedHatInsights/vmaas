6d2aeb47b353675dca3e0a375fb302e0bc808d29
Andrii But <abut@redhat.com>
2020-02-24 15:56:38 +0100

    feat(webapp): add errata filtering by severity and errata type

--- webapp/errata.py
+++ webapp/errata.py
@@ -41,7 +41,44 @@ class ErrataAPI:
                 filtered_errata_to_process.append(errata)
         return filtered_errata_to_process
 
-    def process_list(self, api_version, data): # pylint: disable=unused-argument
+    def _filter_errata_by_prop(self, errata_to_process: list, prop: str, values: list) -> list:
+        """
+        Filter errata by property: severity, type, etc.
+        :return list of filtered errata
+        """
+        # if not isinstance(value, list):
+        #     value = [value]
+        if prop == 'type':
+            prop = ERRATA_TYPE
+        elif prop == 'severity':
+            prop = ERRATA_SEVERITY
+        else:
+            return errata_to_process
+
+        filtered_errata_to_process = []
+        for errata in errata_to_process:
+            errata_detail = self.cache.errata_detail.get(errata)
+            if not errata_detail:
+                continue
+            if errata_detail[prop] in values:
+                filtered_errata_to_process.append(errata)
+        return filtered_errata_to_process
+
+    @staticmethod
+    def _prepare_severity(severity):
+        if isinstance(severity, str):
+            ret = [severity.capitalize()]
+        elif isinstance(severity, list) and severity.count(None) != len(severity):
+            ret = [s.capitalize() for s in set(severity) if s is not None]
+            if None in severity:
+                ret.append(None)
+        elif severity is None or severity.count(None) == len(severity):
+            ret = [None]
+        else:
+            ret = severity
+        return ret
+
+    def process_list(self, api_version, data):  # pylint: disable=unused-argument
         """
         This method returns details for given set of Errata.
 
@@ -54,10 +91,26 @@ class ErrataAPI:
         errata_to_process = data.get("errata_list", None)
         page = data.get("page", None)
         page_size = data.get("page_size", None)
+        errata_type = data.get("type", None)
+        severity = data.get("severity", [])
 
         response = {"errata_list": {}}
+        filters = [(filter_item_if_exists, [self.cache.errata_detail])]
         if modified_since:
             response["modified_since"] = modified_since
+            # if we have information about modified/published dates and receive "modified_since" in request,
+            # compare the dates
+            filters.append((self._filter_modified_since, [modified_since_dt]))
+        if errata_type:
+            errata_type = [t.lower() for t in set(errata_type)] \
+                if isinstance(errata_type, list) else [errata_type.lower()]
+            response["type"] = errata_type
+            filters.append((self._filter_errata_by_prop, ["type", errata_type]))
+
+        if severity is None or len(severity) != 0:
+            severity = self._prepare_severity(severity)
+            response["severity"] = severity
+            filters.append((self._filter_errata_by_prop, ["severity", severity]))
 
         if not errata_to_process:
             return response
@@ -65,12 +118,6 @@ class ErrataAPI:
         if len(errata_to_process) == 1:
             # treat single-label like a regex, get all matching names
             errata_to_process = self.find_errata_by_regex(errata_to_process[0])
-
-        filters = [(filter_item_if_exists, [self.cache.errata_detail])]
-        # if we have information about modified/published dates and receive "modified_since" in request,
-        # compare the dates
-        if modified_since:
-            filters.append((self._filter_modified_since, [modified_since_dt]))
 
         errata_list = {}
         errata_page_to_process, pagination_response = paginate(errata_to_process, page, page_size, filters=filters)
@@ -104,8 +151,7 @@ class ErrataAPI:
                 "bugzilla_list": errata_detail[ERRATA_BUGZILLA],
                 "reference_list": errata_detail[ERRATA_REFERENCE],
                 "modules_list": errata_detail[ERRATA_MODULE],
-                "url": none2empty(errata_detail[ERRATA_URL])
-                }
+                "url": none2empty(errata_detail[ERRATA_URL])}
         response["errata_list"] = errata_list
         response.update(pagination_response)
         return response
