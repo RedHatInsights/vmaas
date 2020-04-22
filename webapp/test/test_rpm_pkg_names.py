f9c59230eb6650cd23a9032a943f5f051b4c015b
Tomáš Sasák <tsasak@redhat.com>
2020-04-22 15:38:58 +0200

    fix(webapp): distinguish existining and nonex. packages on /rpms

--- webapp/test/test_rpm_pkg_names.py
+++ webapp/test/test_rpm_pkg_names.py
@@ -16,13 +16,15 @@ RPM_NAMES_JSON = {"rpm_name_list": [RPM]}
 RPM_SRPM_NAMES_JSON = {"rpm_name_list": [RPM]}
 RPM_CS_JSON = {"rpm_name_list": [RPM], "content_set_list": [CS_LABEL]}
 RPM_JSON_EMPTY_LIST = {"rpm_name_list": [""]}
-RPM_CS_JSON_EMPTY_LIST = {"rpm_name_list": [""], "content_set": [""]}
+RPM_CS_JSON_EMPTY_LIST = {"rpm_name_list": [""], "content_set_list": [""]}
 RPM_JSON_NON_EXIST = {"rpm_name_list": [NONE_EXIST]}
-RPM_CS_JSON_NON_EXIST = {"rpm_name_list": [NONE_EXIST], "content_set": [NONE_EXIST]}
+RPM_CS_JSON_NON_EXIST = {"rpm_name_list": [NONE_EXIST], "content_set_list": [NONE_EXIST]}
+RPM_CS_NONEXIST = {"rpm_name_list": [RPM], "content_set_list": [NONE_EXIST]}
 
 LAST_CHANGE = "2019-03-07T09:17:23.799995"
-EMPTY_RPM_RESPONSE = {"": []}
+EMPTY_RPM_RESPONSE = {}
 NON_EXIST_RPM_RESPONSE = {"none-exist": []}
+EXIST_RPM_RESPONSE_EMPTY_CS = {f"{RPM}": []}
 
 
 class TestRPMPkgNamesAPI(TestBase):
@@ -55,7 +57,12 @@ class TestRPMPkgNamesAPI(TestBase):
     def test_non_existing_rpms(self):
         """Test PackageNamesAPI with non existing rpms."""
         rpm_resp = self.package_names_api.process_list(1, RPM_JSON_NON_EXIST)
-        assert NON_EXIST_RPM_RESPONSE == rpm_resp['rpm_name_list']
+        assert EMPTY_RPM_RESPONSE == rpm_resp['rpm_name_list']
+
+    def test_noex_content_set_ex_srpm(self):
+        """Test PackageNamesAPI with existing rpm and nonexisting content set."""
+        rpm_resp = self.package_names_api.process_list(1, RPM_CS_NONEXIST)
+        assert EXIST_RPM_RESPONSE_EMPTY_CS == rpm_resp["rpm_name_list"]
 
     def test_last_change_in_resp(self):
         """Test PackageNamesAPI response contains last_change."""
