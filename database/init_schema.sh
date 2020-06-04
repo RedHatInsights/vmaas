9be7e4cd3b3d0ce22599851800385951a2735062
Jan Dobes <jdobes@redhat.com>
2020-06-04 17:22:57 +0200

    refactor(database): initialize schema if database container is not present (RDS)

--- database/init_schema.sh
+++ database/init_schema.sh
@@ -5,6 +5,8 @@ if $PG_INITIALIZED ; then
     cat ${CONTAINER_SCRIPTS_PATH}/start/vmaas_db_postgresql.sql | psql -U ${POSTGRESQL_USER} -d ${POSTGRESQL_DATABASE}
     psql -c "ALTER USER vmaas_writer WITH PASSWORD '${POSTGRESQL_WRITER_PASSWORD}'" -d ${POSTGRESQL_DATABASE}
     psql -c "ALTER USER vmaas_reader WITH PASSWORD '${POSTGRESQL_READER_PASSWORD}'" -d ${POSTGRESQL_DATABASE}
+    # Need to make sure admin role has createrole attribute
+    psql -c "ALTER USER ${POSTGRESQL_USER} WITH CREATEROLE" -d ${POSTGRESQL_DATABASE}
 else
     echo "Schema initialization skipped."
 fi
--- database/upgrade/dbupgrade.sh
+++ database/upgrade/dbupgrade.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 
+function psql_exec {
+    PGPASSWORD="${POSTGRESQL_PASSWORD}" psql --no-password -h "${POSTGRESQL_HOST}" -p "${POSTGRESQL_PORT}" -U "${POSTGRESQL_USER}" -d "${POSTGRESQL_DATABASE}" -t -f "$1"
+}
+
 cd $(dirname $0)
 
-exec ./wait-for-services.sh python3 -m database.upgrade
+# Try to initialize schema if there are no tables (typically first run in deployments without database container - RDS)
+EXISTING_TABLES=$(echo "select count(*) from pg_stat_user_tables" | psql_exec - | sed 's/[[:space:]]//g')
+RETVAL=$?
+if [[ "$RETVAL" == "0" && "$EXISTING_TABLES" == "0" ]]; then
+    echo "Empty database, initializing..."
+    psql_exec ./vmaas_user_create_postgresql.sql
+    psql_exec ./vmaas_db_postgresql.sql
+    echo "ALTER USER vmaas_writer WITH PASSWORD '${POSTGRESQL_WRITER_PASSWORD:-vmaas_writer_pwd}'" | psql_exec -
+    echo "ALTER USER vmaas_reader WITH PASSWORD '${POSTGRESQL_READER_PASSWORD:-vmaas_reader_pwd}'" | psql_exec -
+else
+    exec ./wait-for-services.sh python3 -m database.upgrade
+fi
