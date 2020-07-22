aa21829807ced2b2339834972e2dd52f92a19497
Jan Dobes <jdobes@redhat.com>
2020-07-22 13:38:50 +0200

    refactor: use single dockerfile for all app services

---
+++ entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+cd $(dirname $0)
+
+if [[ ! -z $1 ]]; then
+    if [[ "$1" == "webapp" ]]; then
+        cd webapp
+        exec ../wait-for-services.sh python3 -m main
+    elif [[ "$1" == "reposcan" ]]; then
+        rsync --daemon --verbose
+        cd reposcan
+        exec ../wait-for-services.sh python3 -m main
+    elif [[ "$1" == "websocket" ]]; then
+        cd websocket
+        exec python3 -m websocket
+    elif [[ "$1" == "webapp-utils" ]]; then
+        cd webapp_utils
+        exec gunicorn -c gunicorn_conf.py -w ${GUNICORN_WORKERS:-4} --bind=0.0.0.0:8083 app
+    fi
+fi
+
+echo "Please specify service name as the first argument."
