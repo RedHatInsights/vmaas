8ef4337a431f13a2c312bd87a64c9e933c8e1c89
Jan Dobes <jdobes@redhat.com>
2020-09-08 13:09:10 +0100

    Merge pull request #749 from jdobes/gracefuly_exit_sleep
    fix: gracefully exit sleeping scripts

--- wait-for-services.sh
+++ wait-for-services.sh
@@ -2,6 +2,8 @@
 # wait-for-services.sh
 # modified version from https://docs.docker.com/compose/startup-order/
 
+trap 'echo "Stopped."; exit 1' SIGINT SIGTERM
+
 set -e
 
 cmd="$@"
@@ -10,7 +12,8 @@ if [ ! -z "$POSTGRESQL_DATABASE" ]; then
   >&2 echo "Checking if PostgreSQL is up"
   until python3 -c "import psycopg2;c=psycopg2.connect(host=\"$POSTGRESQL_HOST\",database=\"$POSTGRESQL_DATABASE\",user=\"$POSTGRESQL_USER\",port=\"$POSTGRESQL_PORT\",password=\"$POSTGRESQL_PASSWORD\");c.close()" &> /dev/null; do
     >&2 echo "PostgreSQL is unavailable - sleeping"
-    sleep 1
+    sleep 1 &
+    wait $!
   done
 else
   >&2 echo "Skipping PostgreSQL check"
@@ -20,7 +23,8 @@ if [ ! -z "$VMAAS_WEBSOCKET_HOST" ]; then
   >&2 echo "Checking if websocket server is up"
   until curl http://$WEBSOCKET_HOST:8082/api/v1/monitoring/health &>/dev/null; do
     >&2 echo "Websocket server is unavailable - sleeping"
-    sleep 1
+    sleep 1 &
+    wait $!
   done
 else
   >&2 echo "Skipping websocket server check"
@@ -30,7 +34,8 @@ if [ ! -z "$REPOSCAN_HOST" ]; then
   >&2 echo "Checking if reposcan API is up"
   until curl http://$REPOSCAN_HOST:8081/api/v1/monitoring/health &>/dev/null; do
     >&2 echo "Reposcan API is unavailable - sleeping"
-    sleep 1
+    sleep 1 &
+    wait $!
   done
 else
   >&2 echo "Skipping reposcan API check"
