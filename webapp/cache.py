fe2bd0eb7215314cd7bcd0e3ea2a8a708fae3175
mhornick <mhornick@redhat.com>
2021-04-13 11:36:44 +0200

    feat: add sqlite database format
    Extract sqlite dump generation code from the semtezv/next branch

--- webapp/cache.py
+++ webapp/cache.py
@@ -6,6 +6,7 @@ import asyncio
 import dbm
 import os
 import shelve
+import sqlite3
 
 from common.config import Config
 from common.logging_utils import get_logger
@@ -145,6 +146,122 @@ class Cache:
     def download():
         """Download new version of data."""
         return not os.system("rsync -a --copy-links --quiet %s %s" % (REMOTE_DUMP, DUMP))
+
+    # pylint: disable=too-many-branches,redefined-builtin,broad-except,invalid-name
+    def _load_sqlite(self, data):
+
+        for (id, arch) in data.execute('select id, arch from arch'):
+            self.id2arch[int(id)] = arch
+            self.arch2id[arch] = int(id)
+
+        for (src, dst) in data.execute('select from_arch_id, to_arch_id from arch_compat'):
+            self.arch_compat[int(src)] = int(dst)
+
+        for (id, string) in data.execute('select id, string from string'):
+            self.strings[int(id)] = string
+
+        for (id, label) in data.execute('select id, label from content_set'):
+            self.content_set_id2label[id] = label
+            self.label2content_set_id[label] = id
+
+        for (id, name) in data.execute('select id, packagename from packagename'):
+            self.packagename2id[str(name)] = int(id)
+            self.id2packagename[int(id)] = str(name)
+
+        for (cs_id, name_id) in data.execute("select content_set_id, pkg_name_id from content_set_pkg_name"):
+            self.content_set_id2pkg_name_ids.setdefault(cs_id, []).append(name_id)
+
+        for (cs_id, src_name_id) in data.execute(
+                "select content_set_id, src_pkg_name_id from content_set_src_pkg_name"):
+            self.src_pkg_name_id2cs_ids.setdefault(src_name_id, []).append(cs_id)
+
+        for (cpe_id, label) in data.execute("select id, label from cpe"):
+            self.cpe_id2label[cpe_id] = label
+            self.label2cpe_id[label] = cpe_id
+
+        for (cpe_id, cs_id) in data.execute("select cpe_id, content_set_id from cpe_content_set"):
+            self.content_set_id2cpe_ids.setdefault(cs_id, []).append(cpe_id)
+
+        for (name_id, pkg_id) in data.execute(
+                'select name_id, package_id from updates order by name_id, package_id, package_order'):
+            self.updates.setdefault(int(name_id), []).append(int(pkg_id))
+
+        for (name_id, evr_id, order) in data.execute(
+                'select name_id, evr_id, package_order from updates_index order by package_order'):
+            name_id = int(name_id)
+            evr_id = int(evr_id)
+            order = int(order)
+            self.updates_index.setdefault(name_id, dict()).setdefault(evr_id, []).append(order)
+
+        for (id, epoch, ver, rel) in data.execute('select id, epoch, version, release from evr'):
+            evr = (str(epoch), str(ver), str(rel))
+            self.id2evr[int(id)] = evr
+            self.evr2id[evr] = int(id)
+
+        for (id, name_id, evr_id, arch_id, sum_id, descr_id, src_pkg_id) in data.execute(
+                'select * from package_detail'):
+            self.package_details[id] = (name_id, evr_id, arch_id, sum_id, descr_id, src_pkg_id)
+            self.nevra2pkgid[(name_id, evr_id, arch_id)] = id
+
+            if src_pkg_id:
+                self.src_pkg_id2pkg_ids.setdefault(src_pkg_id, []).append(id)
+
+        for row in data.execute("select * from repo_detail"):
+            id = row[0]
+            repo = row[1:]
+            self.repo_detail[id] = repo
+            self.repolabel2ids.setdefault(repo[0], []).append(id)
+            self.productid2repoids.setdefault(repo[-2], []).append(id)
+
+        for row in data.execute("select pkg_id, repo_id from pkg_repo"):
+            self.pkgid2repoids.setdefault(row[0], array.array('q')).append(row[1])
+
+        for row in data.execute("select * from errata_detail"):
+            id = row[0]
+            name = row[1]
+            errata = row[2:]
+            self.errata_detail[name] = errata
+            self.errataid2name[id] = name
+
+        for row in data.execute("select errata_id, repo_id from errata_repo"):
+            self.errataid2repoids.setdefault(row[0], []).append(row[1])
+
+        for row in data.execute("select pkg_id, errata_id from pkg_errata "):
+            self.pkgid2errataids.setdefault(row[0], []).append(row[1])
+
+        for row in data.execute("select pkg_id, errata_id, module_stream_id from errata_modulepkg"):
+            self.pkgerrata2module.setdefault((row[0], row[1]), set()).add(row[2])
+
+        for row in data.execute("select module, stream, stream_id from module_stream"):
+            self.modulename2id[(row[0], row[1])] = row[2]
+
+        for row in data.execute("select * from cve_detail"):
+            name = row[1]
+            item = row[2:]
+            self.cve_detail[name] = item
+
+        names = ["exported", "last_change", "repository_changes", "cve_changes", "errata_changes"]
+
+        for row in data.execute("select %s from dbchange" % ','.join(names)):
+            for (i, v) in enumerate(row):
+                self.dbchange[names[i]] = v
+
+        LOGGER.info("Loaded cache")
+
+    def load_sqlite(self, filename):
+        """Load data from sqlite file into dictionaries."""
+        # pylint: disable=too-many-branches,too-many-statements
+        LOGGER.info("Loading sqlite cache dump...")
+        try:
+            with sqlite3.connect(filename) as data:
+                self._load_sqlite(data)
+        except sqlite3.Error as err:
+            # file does not exist or has wrong type
+            LOGGER.warning("Failed to load data %s: %s", filename, err)
+            return
+        except Exception as err:
+            LOGGER.warning("Failed to load data %s: %s", filename, err)
+            return
 
     def load(self, filename):
         """Load data from shelve file into dictionaries."""
