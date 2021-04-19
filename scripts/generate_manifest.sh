62c8dff6cabaaa3b26ea884fa38a9cebfcf72622
Jan Dobes <jdobes@redhat.com>
2021-04-19 17:33:38 +0200

    chore: update to current manifest format

--- scripts/generate_manifest.sh
+++ scripts/generate_manifest.sh
@@ -1,36 +1,34 @@
 #!/bin/sh
 
-# ./scripts/generate_manifest.sh <MANIFEST_PATH> <PREFIX> <BASE_RPM_LIST> <FINAL_RPM_LIST> <PYTHON-CMD-OPTIONAL>
+# ./scripts/generate_manifest.sh <MANIFEST_PATH> <PREFIX> <BASE_IMAGE> <BASE_RPM_LIST> <FINAL_RPM_LIST>
 # Example:
-# ./scripts/generate_manifest.sh manifest_webapp.txt my-service /tmp/base_rpm_list.txt /tmp/final_rpm_list.txt python
-# cat manifest_webapp.txt
+# ./scripts/generate_manifest.sh manifest.txt my-service OCI_ubi-minimal registry.access.redhat.com/ubi8 /tmp/base_rpm_list.txt /tmp/final_rpm_list.txt python3
+# cat manifest.txt
 
 MANIFEST_PATH=$1
 PREFIX=$2
-BASE_RPM_LIST=$3
-FINAL_RPM_LIST=$4
-PYTHON=$5
+BASE_IMAGE=$3
+BASE_RPM_LIST=$4
+FINAL_RPM_LIST=$5
 
-grep -v -f ${BASE_RPM_LIST} ${FINAL_RPM_LIST} > ${MANIFEST_PATH}
+echo "${BASE_IMAGE}" > ${MANIFEST_PATH} # base image
 
-## Write Python packages if python set.
-if [[ ! -z $PYTHON ]]
+grep -v -f ${BASE_RPM_LIST} ${FINAL_RPM_LIST} >> ${MANIFEST_PATH} # rpms different from base image
+
+# Write Python packages if python is available.
+if [[ -x "$(command -v python3)" ]]
 then
-    $PYTHON -m pip freeze | sort > /tmp/pipdeps
-    sed -i -e 's/^/'$PYTHON'-/' /tmp/pipdeps  # add 'python' prefix
+    python3 -m pip freeze | sort > /tmp/pipdeps
     sed -i -e 's/==/-/' /tmp/pipdeps       # replace '==' with '-'
-    sed -i -e 's/$/.pipfile/' /tmp/pipdeps # add '.pipfile' suffix
+    sed -i -e 's/$/.pypi/' /tmp/pipdeps    # add '.pypi' suffix
     cat /tmp/pipdeps >> ${MANIFEST_PATH}   # append python deps to manifest
 fi
 
-## Add prefix to all lines.
-sed -i -e 's/^/'${PREFIX}'/' ${MANIFEST_PATH}
-
-## Write precise version only in prod builds.
-if [[ "$MANIFEST_WRITE_VERSION" == "TRUE" ]]
+# Add prefix to all lines.
+VERSION="$(/get_app_version.sh)"
+if [[ ! -z "$VERSION" ]]
 then
-    VERSION=$(${PYTHON} -c 'from common.constants import VMAAS_VERSION;print(VMAAS_VERSION)')
-    sed -i -e 's/:VERSION:/:'$VERSION':/' ${MANIFEST_PATH}
+    sed -i -e 's/^/'${PREFIX}\:${VERSION}\\/'/' ${MANIFEST_PATH}
 else
-    sed -i -e 's/:VERSION:/:latest:/' ${MANIFEST_PATH}
+    sed -i -e 's/^/'${PREFIX}\\/'/' ${MANIFEST_PATH}
 fi
