ecb800505d0290a7e8a37c7eda4755cb76323a21
Tomáš Sasák <tsasak@redhat.com>
2021-11-08 15:23:51 +0100

    chore: add metrics ports for clowder
    VULN-1387

--- vmaas/reposcan/main.py
+++ vmaas/reposcan/main.py
@@ -2,6 +2,7 @@
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
 from tornado.wsgi import WSGIContainer
+from prometheus_client import start_http_server
 
 from vmaas.common.config import Config
 from vmaas.reposcan.reposcan import create_app, DEFAULT_PATH, DEFAULT_PATH_API
@@ -15,4 +16,5 @@ if __name__ == '__main__':
     cfg = Config()
     server = HTTPServer(WSGIContainer(application))
     server.listen(cfg.public_port or cfg.reposcan_port)
+    start_http_server(int(cfg.metrics_port))
     IOLoop.instance().start()
--- vmaas/webapp/main.py
+++ vmaas/webapp/main.py
@@ -2,6 +2,7 @@
 import asyncio
 
 from aiohttp import web
+from prometheus_client import start_http_server
 from vmaas.webapp.app import create_app, DEFAULT_PATH, DEFAULT_PATH_API
 from vmaas.webapp.app import init_websocket
 
@@ -24,4 +25,5 @@ if __name__ == '__main__':
     cfg = Config()
     port = cfg.web_port or cfg.webapp_port
 
+    start_http_server(int(cfg.metrics_port))
     web.run_app(application.app, port=port, access_log_format="%s %r (%a) %Tfs", loop=asyncio.get_event_loop())
