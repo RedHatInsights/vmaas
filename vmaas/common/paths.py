ef444cd94478aa1b7d21076acf39dfd8e31e8114
Josef Hak <jhak@redhat.com>
2021-12-17 20:38:58 +0100

    chore: Added common.paths file to define file paths constants
    - VMAAS-1403

---
+++ vmaas/common/paths.py
@@ -0,0 +1,9 @@
+"""
+Commonly used constant paths (sql scripts etc.)
+"""
+from pathlib import Path
+
+VMAAS_DIR = Path(__file__).resolve().parent.parent.parent
+DB_UPGRADES_PATH = VMAAS_DIR.joinpath("database", "upgrade_scripts")
+DB_CREATE_SQL_PATH = VMAAS_DIR.joinpath("database", "vmaas_db_postgresql.sql")
+USER_CREATE_SQL_PATH = VMAAS_DIR.joinpath("database", "vmaas_user_create_postgresql.sql")
