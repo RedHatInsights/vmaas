a328145c83f7541eef94eb4448f413ef4e481e4a
Patrik Segedy <psegedy@redhat.com>
2022-04-13 13:41:00 +0200

    [DON'T MERGE] try python cicd

--- pr_check.sh
+++ pr_check.sh
@@ -3,12 +3,12 @@
 # --------------------------------------------
 # Options that must be configured by app owner
 # --------------------------------------------
-APP_NAME="vulnerability"  # name of app-sre "application" folder this component lives in
-COMPONENT_NAME="vmaas"  # name of app-sre "resourceTemplate" in deploy.yaml for this component
-IMAGE="quay.io/cloudservices/vmaas-app"  
-COMPONENTS="vmaas"
-COMPONENTS_W_RESOURCES="vmaas"
-CACHE_FROM_LATEST_IMAGE="true"
+export APP_NAME="vulnerability"  # name of app-sre "application" folder this component lives in
+export COMPONENT_NAME="vmaas"  # name of app-sre "resourceTemplate" in deploy.yaml for this component
+export IMAGE="quay.io/cloudservices/vmaas-app"  
+export COMPONENTS="vmaas"
+export COMPONENTS_W_RESOURCES="vmaas"
+export CACHE_FROM_LATEST_IMAGE="true"
 
 export IQE_PLUGINS="vmaas"
 export IQE_MARKER_EXPRESSION=""
@@ -24,121 +24,19 @@ export ARTIFACTS_DIR="$WORKSPACE/artifacts"
 mkdir -p $ARTIFACTS_DIR
 LABELS_DIR="$WORKSPACE/github_labels"
 
-mkdir -p $LABELS_DIR
-exit_code=0
-task_arr=([1]="Build" [2]="Deploy" [3]="Smoke Tests")
-error_arr=([1]="The PR is labeled to not build the test image" \
-           [2]="The PR is labeled to not deploy to ephemeral" \
-           [3]="The PR is labeled to not run smoke tests")
 
-function check_for_labels() {
-    if [ -f $LABELS_DIR/github_labels.txt ]; then
-        egrep "$1" $LABELS_DIR/github_labels.txt &>/dev/null
-    fi
-}
+# Install bonfire repo/initialize
+CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
+curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh && source .cicd_bootstrap.sh
+echo "creating PR image"
 
-function _process_requirements_labels() {
-    # $1 env var to export
-    # $@ labels to process
-    env_var_name=$1; shift;
-    labels=$@
-    if [ -n "$labels" ]; then
-        labels=$(echo "$labels" | sed -e 's/"//g')
-        # contents of labels is different in different shells
-        # it can be one of
-        #   labels="REQ1\nREQ2"
-        #   labels="REQ1 REQ2"
-        lines=$(echo "$labels" | wc -l | xargs)
-        words=$(echo "$labels" | wc -w | xargs)
-        if [[ "$lines" == "1" ]] && [[ "$words" == "1" ]]; then
-            export "$env_var_name=$labels"
-            return
-        fi
-        processed=""
-        for req in $labels; do
-            processed="$processed$req,"
-        done
-        # delete extra comma
-        processed="${processed%?}"
-        export "$env_var_name=$processed"
-    fi
-}
+git clone --branch python_cicd https://github.com/psegedy/bonfire.git
+git clone --branch podman_client https://github.com/RedHatInsights/bonfire-cicd.git
+echo "pip install bonfire"
+python3 -m pip install bonfire/
+echo "python3 -m pip install bonfire-cicd"
+python3 -m pip install bonfire-cicd/
 
-function process_requirements_labels() {
-    if [ -f $LABELS_DIR/github_labels.txt ]; then
-        requirements=$(egrep "^\"[A-Z]+-[A-Z-]*\"$" $LABELS_DIR/github_labels.txt)
-        requirements_priority=$(egrep "^\"(critical|high|medium|low)-requirements\"$" $LABELS_DIR/github_labels.txt | sed -e 's/-requirements//g')
-        _process_requirements_labels IQE_REQUIREMENTS $requirements
-        _process_requirements_labels IQE_REQUIREMENTS_PRIORITY $requirements_priority
-    fi
-}
-
-function build_image() {
-    source $CICD_ROOT/build.sh
-}
-
-function deploy_ephemeral() {
-    source $CICD_ROOT/deploy_ephemeral_env.sh
-}
-
-function run_smoke_tests() {
-    source $CICD_ROOT/cji_smoke_test.sh
-}
-
-function make_results_xml() {
-cat << EOF > $WORKSPACE/artifacts/junit-pr_check.xml
-<?xml version="1.0" encoding="UTF-8" ?>
-<testsuite id="pr_check" name="PR Check" tests="1" failures="0">
-    <testcase id="pr_check.${task_arr[$exit_code]}" name="${task_arr[$exit_code]}">
-    </testcase>
-</testsuite>
-EOF
-}
-
-# Save PR labels into a file
-set -ex
-# GH_API_URL="${GITHUB_API_URL:-https://api.github.com/}"  # don't use app-sre github api mirror for now
-curl -s -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/search/issues\?q\=sha:$ghprbActualCommit | jq '.items[].labels[].name' > $LABELS_DIR/github_labels.txt
-
-if check_for_labels "keep-namespace"; then
-    export RELEASE_NAMESPACE=false
-fi
-
-if check_for_labels "skip-build"; then
-    echo "PR check skipped"
-    exit_code=1
-else
-    # Install bonfire repo/initialize
-    CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
-    curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh && source .cicd_bootstrap.sh
-    echo "creating PR image"
-    build_image
-fi
-
-if [[ $exit_code == 0 ]]; then
-    if check_for_labels "skip-deploy"; then
-        echo "deployment to ephemeral skipped"
-        exit_code=2
-    else
-        echo "deploying to ephemeral"
-        deploy_ephemeral
-        if check_for_labels "skip-tests"; then
-            echo "PR smoke tests skipped"
-            exit_code=3
-        else
-            echo "running PR smoke tests"
-            set +e
-            process_requirements_labels
-            set -e
-            run_smoke_tests
-        fi
-    fi
-fi
-
-cp $LABELS_DIR/github_labels.txt $ARTIFACTS_DIR/github_labels.txt
-
-if [[ $exit_code != 0 ]]
-then
-    echo "PR check failed"
-    make_results_xml
-fi
+bonfire cicd build
+bonfire cicd deploy ephemeral
+bonfire cicd smoke-tests
