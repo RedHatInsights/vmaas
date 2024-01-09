500d194b7a546d0075c08228aa7154a6fbf1722a
Jan Dobes <jdobes@redhat.com>
2024-01-09 14:34:46 +0100

    chore: migrate from pipenv to poetry

--- scripts/check_deps_versions.sh
+++ scripts/check_deps_versions.sh
@@ -4,13 +4,13 @@ CYAN='\033[0;36m'
 NO_COLOR='\033[0m'
 echo -e "${CYAN}Dependency updates checking...${NO_COLOR}"
 
-# Check for python deps updates (all dirs containing Pipfile).
-for APP_DIR in $(find -name Pipfile -printf '%h ')
+# Check for python deps updates (all dirs containing poetry.lock).
+for APP_DIR in $(find -name poetry.lock -printf '%h ')
 do
     echo "Analyzing deps in '$APP_DIR'"
     cd $APP_DIR
-    pipenv run pip freeze > /tmp/req.txt
-    pipenv run pur -r /tmp/req.txt -o /dev/null | grep "Updated" | sed -e "s/Updated/You can update:/g"
+    pip freeze > /tmp/req.txt
+    pur -r /tmp/req.txt -o /dev/null | grep "Updated" | sed -e "s/Updated/You can update:/g"
     cd -
 done
 echo -e "${CYAN}------------------------------${NO_COLOR}"
