971fc9256fe180d14dab7122544f0738b062ada4
michalslomczynski <mslomczy@redhat.com>
2024-01-11 21:55:36 +0100

    feat(reposcan): add CSAF sync task

--- vmaas/reposcan/mnm.py
+++ vmaas/reposcan/mnm.py
@@ -42,5 +42,6 @@ OVAL_FAILED_DELETE = Counter('vmaas_reposcan_failed_oval_delete', '# of failed o
 
 CSAF_FAILED_DOWNLOAD = Counter('vmaas_reposcan_failed_csaf_download', '# of failed csaf-download attempts')
 CSAF_FAILED_DELETE = Counter('vmaas_reposcan_failed_csaf_delete', '# of failed csaf-delete attempts')
+CSAF_FAILED_IMPORT = Counter('vmaas_reposcan_failed_csaf_import', '# of failed csaf-import attempts')
 
 REPOS_TO_CLEANUP = Gauge('vmaas_reposcan_repos_cleanup', '# of repos to cleanup from DB')
