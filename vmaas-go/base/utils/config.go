ebda8682a3586234a779ef6f9b60ec4eba595350
Jakub Dugovič <jdugovic@redhat.com>
2025-02-12 11:51:34 +0100

    chore: replace exportloopref with copyloopvar
    Since Go 1.22, loop vars don't need to be copied and exportloopref is
    deprecated.

--- vmaas-go/base/utils/config.go
+++ vmaas-go/base/utils/config.go
@@ -100,7 +100,6 @@ func initAPI() {
 
 func initEndpoints() {
 	for _, e := range clowder.LoadedConfig.Endpoints {
-		e := e // re-assign iteration variable to use a new memory pointer
 		if e.App == "vmaas" {
 			switch {
 			case strings.Contains(e.Name, "reposcan"):
@@ -111,7 +110,6 @@ func initEndpoints() {
 		}
 	}
 	for _, e := range clowder.LoadedConfig.PrivateEndpoints {
-		e := e // re-assign iteration variable to use a new memory pointer
 		if e.App == "vmaas" && strings.Contains(e.Name, "reposcan") {
 			Cfg.DumpAddress = fmt.Sprintf("%s/vmaas.db", (*PrivateEndpoint)(&e).BuildURL("http"))
 		}
