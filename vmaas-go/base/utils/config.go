851d5dde6f7349272631588bb409b71a03c5eaf3
Viliam Krizan <vkrizan@redhat.com>
2025-06-06 12:26:31 +0200

    feat(go): Clowder config independence on WebappGo

--- vmaas-go/base/utils/config.go
+++ vmaas-go/base/utils/config.go
@@ -3,15 +3,21 @@ package utils
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 
 	clowder "github.com/redhatinsights/app-common-go/pkg/api/v1"
 )
 
+const DumpPath = "/vmaas.db"
+
 var Cfg = Config{}
 
 type Config struct {
+	// Clowder
+	isClowder bool
+
 	// database
 	DBHost          string
 	DBName          string
@@ -60,9 +66,7 @@ type (
 )
 
 func ConfigureConfig() {
-	if !clowder.IsClowderEnabled() {
-		panic("Missing clowder config")
-	}
+	Cfg.isClowder = clowder.IsClowderEnabled()
 	Cfg.initDB()
 	Cfg.initAPI()
 	Cfg.initEndpoints()
@@ -71,49 +75,95 @@ func ConfigureConfig() {
 }
 
 func (c *Config) initDB() {
-	c.DBHost = clowder.LoadedConfig.Database.Hostname
-	c.DBName = clowder.LoadedConfig.Database.Name
-	c.DBPort = clowder.LoadedConfig.Database.Port
-	c.DBSslMode = clowder.LoadedConfig.Database.SslMode
-	if clowder.LoadedConfig.Database.RdsCa != nil {
-		certPath, err := clowder.LoadedConfig.RdsCa()
-		if err != nil {
-			panic(err)
+	if c.isClowder {
+		c.DBHost = clowder.LoadedConfig.Database.Hostname
+		c.DBName = clowder.LoadedConfig.Database.Name
+		c.DBPort = clowder.LoadedConfig.Database.Port
+		c.DBSslMode = clowder.LoadedConfig.Database.SslMode
+		if clowder.LoadedConfig.Database.RdsCa != nil {
+			certPath, err := clowder.LoadedConfig.RdsCa()
+			if err != nil {
+				panic(err)
+			}
+			c.DBSslRootCert = certPath
 		}
-		c.DBSslRootCert = certPath
+		c.DBAdminUser = clowder.LoadedConfig.Database.AdminUsername
+		c.DBAdminPassword = clowder.LoadedConfig.Database.AdminPassword
+		c.DBUser = clowder.LoadedConfig.Database.Username
+		c.DBPassword = clowder.LoadedConfig.Database.Password
+	} else {
+		var err error
+		c.DBHost = os.Getenv("POSTGRESQL_HOST")
+		c.DBName = os.Getenv("POSTGRESQL_DB")
+		c.DBPort, err = strconv.Atoi(Getenv("POSTGRESQL_PORT", "5432"))
+		if err != nil {
+			LogPanic("err", err, "POSTGRESQL_PORT environment variable is not an integer")
+		}
+		c.DBSslMode = Getenv("POSTGRESQL_SSL_MODE", "prefer")
+		c.DBSslRootCert = os.Getenv("POSTGRESQL_SSL_ROOT_CERT_PATH")
+		c.DBUser = os.Getenv("POSTGRESQL_USER")
+		c.DBPassword = os.Getenv("POSTGRESQL_PASSWORD")
 	}
-	c.DBAdminUser = clowder.LoadedConfig.Database.AdminUsername
-	c.DBAdminPassword = clowder.LoadedConfig.Database.AdminPassword
-	c.DBUser = clowder.LoadedConfig.Database.Username
-	c.DBPassword = clowder.LoadedConfig.Database.Password
 }
 
 func (c *Config) initAPI() {
-	c.PublicPort = *clowder.LoadedConfig.PublicPort
-	c.PrivatePort = *clowder.LoadedConfig.PrivatePort
-	c.MetricsPort = clowder.LoadedConfig.MetricsPort
-	c.MetricsPath = clowder.LoadedConfig.MetricsPath
+	if c.isClowder {
+		c.PublicPort = *clowder.LoadedConfig.PublicPort
+		c.PrivatePort = *clowder.LoadedConfig.PrivatePort
+		c.MetricsPort = clowder.LoadedConfig.MetricsPort
+		c.MetricsPath = clowder.LoadedConfig.MetricsPath
+	} else {
+		var err error
+		c.PublicPort, err = strconv.Atoi(Getenv("BIND_PUBLIC_PORT", "8000"))
+		if err != nil {
+			LogPanic("err", err, "BIND_PUBLIC_PORT environment variable is not an integer")
+		}
+
+		c.PrivatePort, err = strconv.Atoi(Getenv("BIND_PRIVATE_PORT", "10000"))
+		if err != nil {
+			LogPanic("err", err, "BIND_PRIVATE_PORT environment variable is not an integer")
+		}
+
+		c.MetricsPort, err = strconv.Atoi(Getenv("PROMETHEUS_PORT", "9000"))
+		if err != nil {
+			LogPanic("err", err, "PROMETHEUS_PORT environment variable is not an integer")
+		}
+		c.MetricsPath = "/metrics"
+	}
 }
 
 func (c *Config) initEndpoints() {
-	for _, e := range clowder.LoadedConfig.Endpoints {
-		if e.App == "vmaas" {
-			switch {
-			case strings.Contains(e.Name, "reposcan"):
-				c.ReposcanAddress = (*Endpoint)(&e).BuildURL("http")
-			case strings.Contains(e.Name, "webapp") && !strings.Contains(e.Name, "go"):
-				c.OGWebappAddress = (*Endpoint)(&e).BuildURL("http")
+	if c.isClowder {
+		for _, e := range clowder.LoadedConfig.Endpoints {
+			if e.App == "vmaas" {
+				switch {
+				case strings.Contains(e.Name, "reposcan"):
+					c.ReposcanAddress = (*Endpoint)(&e).BuildURL("http")
+				case strings.Contains(e.Name, "webapp") && !strings.Contains(e.Name, "go"):
+					c.OGWebappAddress = (*Endpoint)(&e).BuildURL("http")
+				}
 			}
 		}
-	}
-	for _, e := range clowder.LoadedConfig.PrivateEndpoints {
-		if e.App == "vmaas" && strings.Contains(e.Name, "reposcan") {
-			c.DumpAddress = fmt.Sprintf("%s/vmaas.db", (*PrivateEndpoint)(&e).BuildURL("http"))
+		for _, e := range clowder.LoadedConfig.PrivateEndpoints {
+			if e.App == "vmaas" && strings.Contains(e.Name, "reposcan") {
+				c.DumpAddress = fmt.Sprintf("%s%s", (*PrivateEndpoint)(&e).BuildURL("http"), DumpPath)
+			}
 		}
+	} else {
+		c.ReposcanAddress = os.Getenv("REPOSCAN_PUBLIC_URL")
+		if reposcanPrivateURL := os.Getenv("REPOSCAN_PRIVATE_URL"); reposcanPrivateURL != "" {
+			c.DumpAddress = fmt.Sprintf("%s%s", reposcanPrivateURL, DumpPath)
+		}
+
+		c.OGWebappAddress = os.Getenv("WEBAPP_URL")
 	}
 }
 
 func (c *Config) initCloudwatch() {
+	if !c.isClowder {
+		return
+	}
+
 	cwCfg := clowder.LoadedConfig.Logging.Cloudwatch
 	if cwCfg != nil {
 		c.CloudWatchAccessKeyID = cwCfg.AccessKeyId
