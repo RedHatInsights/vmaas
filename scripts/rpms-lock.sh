51851113c91588acdb99a194ad2cba6571cd5ffc
Jan Dobes <jdobes@redhat.com>
2025-10-15 15:33:00 +0200

    chore: script to generate RPM lockfile handling entitlements inside

---
+++ scripts/rpms-lock.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+# Script to lock RPM dependencies in container and copy generated lock file back
+
+workdir="/tmp/rpms-locker/"
+dockerfile="Dockerfile-rpms"
+
+for runtime in podman docker; do
+    cmd=$(command -v $runtime)
+    if [[ "$cmd" != "" ]] && $cmd ps &> /dev/null; then
+        break
+    else
+        echo "Unable to use $runtime"
+        cmd=""
+    fi
+done
+
+if [[ "$cmd" != "" ]]; then
+    echo "Using: $cmd"
+else
+    echo "No container runtime found!"
+    exit 1
+fi
+
+mkdir -p $workdir
+cat <<EOF > $workdir$dockerfile
+FROM registry.access.redhat.com/ubi9/ubi
+RUN dnf install -y python3 python3-pip python3-dnf skopeo rpm git-core
+RUN python3 -m pip install https://github.com/konflux-ci/rpm-lockfile-prototype/archive/refs/heads/main.tar.gz
+EOF
+
+current_dir=$(pwd)
+cd $workdir
+$cmd build --platform linux/amd64 -t rpms-locker -f $dockerfile .
+cd $current_dir
+$cmd run --rm -d --name rpms-locker-container rpms-locker sleep infinity
+
+$cmd exec rpms-locker-container bash -c "mkdir -p /tmp"
+$cmd cp "rpms.in.yaml" rpms-locker-container:"/tmp/"
+$cmd exec -it rpms-locker-container bash -c "subscription-manager register"
+$cmd exec rpms-locker-container bash -c "cp /etc/yum.repos.d/redhat.repo /tmp/ && sed -i \"s/$(uname -m)/\$basearch/g\" /tmp/redhat.repo"
+$cmd exec rpms-locker-container bash -c "rpm-lockfile-prototype --outfile=/tmp/rpms.lock.yaml --image registry.access.redhat.com/ubi9/ubi-minimal:latest /tmp/rpms.in.yaml"
+
+$cmd cp rpms-locker-container:"/tmp/rpms.lock.yaml" "."
+$cmd exec rpms-locker-container bash -c "subscription-manager unregister"
+
+$cmd kill rpms-locker-container
