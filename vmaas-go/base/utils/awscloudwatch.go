a859955953af4178b9639d75b099eda1d60b318e
Jan Dobes <jdobes@redhat.com>
2026-01-12 16:59:22 +0100

    chore: nolint deprecation warning until we migrate to v2

--- vmaas-go/base/utils/awscloudwatch.go
+++ vmaas-go/base/utils/awscloudwatch.go
@@ -8,8 +8,8 @@ import (
 	"os"
 	"time"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws" //nolint:staticcheck
+	"github.com/aws/aws-sdk-go/aws/credentials" //nolint:staticcheck
 	lc "github.com/redhatinsights/platform-go-middlewares/v2/logging/cloudwatch"
 	log "github.com/sirupsen/logrus"
 )
