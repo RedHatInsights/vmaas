8a1e86539d1d6ae1d7b1ba8305fb99a884756fed
Jan Dobes <jdobes@redhat.com>
2026-06-29 13:45:27 +0200

    refactor: use plain golang http server instead of nginx (#1810)
    * refactor: use plain golang http server instead of nginx
    
    RHINENG-26574
    
    * refactor: check availability of data server in /healthz

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,6 @@ public_port () {
     python3.12 -c "import os;import app_common_python as a;print(a.LoadedConfig.publicPort or os.getenv('BIND_PUBLIC_PORT', '8000'))"
 }
 
-private_port () {
-    python3.12 -c "import os;import app_common_python as a;print(a.LoadedConfig.privatePort or os.getenv('BIND_PRIVATE_PORT', '10000'))"
-}
-
 if [[ "$#" == "0" ]]; then
     echo "Please specify service name as the first argument."
     exit 1
@@ -26,10 +22,8 @@ if [[ ! -z $1 ]]; then
         cd go/src/vmaas
         exec ./main webapp
     elif [[ "$1" == "reposcan" ]]; then
+        /vmaas/go/src/vmaas/main reposcan-data-server &
         cd vmaas/reposcan
-        port=$(private_port)
-        cat nginx.conf.template | sed "s/_PORT_/$port/g" > /tmp/nginx.conf
-        nginx -c /tmp/nginx.conf
         port=$(public_port)
         exec python3.12 -m vmaas.common.wait_for_services uvicorn --host 0.0.0.0 --port $port --no-access-log main:app
     elif [[ "$1" == "sleep" ]]; then
